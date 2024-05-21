68270dd592170e0dff0095dbe3e2a6ed176a5a8c
vorhey <jorgeherreraulloa@gmail.com>
2024-05-21 17:52:48 -0400

    removed luansip and changed debug icons

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -3,30 +3,6 @@ return {
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
-    {
-      'L3MON4D3/LuaSnip',
-      build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
-      },
-    },
     {
       'Exafunction/codeium.nvim',
       enabled = true,
@@ -36,7 +12,6 @@ return {
       },
       opts = {},
     },
-    'saadparwaiz1/cmp_luasnip',
     -- Adds other completion capabilities.
     --  nvim-cmp does not ship with all sources by default. They are split
     --  into multiple repos for maintenance purposes.
@@ -52,14 +27,12 @@ return {
     local utils = require 'utils'
     -- Register sources
     local sources = {
+      { name = 'nvim_lsp' },
       { name = 'codeium' },
-      { name = 'nvim_lsp', max_item_count = 10 },
-      { name = 'luasnip' },
       { name = 'path' },
     }
     -- Pass sources to utils setup for additional resources configuration
     utils.setup_cmp_sources(sources)
-    local ls = require 'luasnip'
     cmp.setup {
       ---@diagnostic disable-next-line: missing-fields
       formatting = {
@@ -76,7 +49,6 @@ return {
             codeium = '[Codeium]',
             nvim_lsp = '[LSP]',
             path = '[path]',
-            luasnip = '[snip]',
             nvim_lua = '[lua]',
           }
 
@@ -97,11 +69,6 @@ return {
           vim_item.kind = vim_item.kind .. ' ' .. vim_item.menu
 
           return vim_item
-        end,
-      },
-      snippet = {
-        expand = function(args)
-          ls.lsp_expand(args.body)
         end,
       },
       preselect = 'None',
@@ -125,32 +92,9 @@ return {
         --  Generally you don't need this, because nvim-cmp will display
         --  completions whenever it has completion options available.
         ['<C-Space>'] = cmp.mapping.complete {},
-        -- Think of <c-l> as moving to the right of your snippet expansion.
-        --  So if you have a snippet that's like:
-        --  function $name($args)
-        --    $body
-        --  end
-        --
-        -- <c-l> will move you to the right of each of the expansion locations.
-        -- <c-h> is similar, except moving you backwards.
-        ['<C-l>'] = cmp.mapping(function()
-          if ls.expand_or_locally_jumpable() then
-            ls.expand_or_jump()
-          end
-        end, { 'i', 's' }),
-        ['<C-h>'] = cmp.mapping(function()
-          if ls.locally_jumpable(-1) then
-            ls.jump(-1)
-          end
-        end, { 'i', 's' }),
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-
         ['<Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif require('luasnip').expand_or_jumpable() then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
           else
             fallback()
           end
@@ -159,8 +103,6 @@ return {
         ['<S-Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif require('luasnip').jumpable(-1) then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
           else
             fallback()
           end
