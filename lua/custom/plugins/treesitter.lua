d2d25ce2aeb29e1b756b15df97118db224d13332
vorhey <jorgeherreraulloa@gmail.com>
2024-07-06 11:23:22 -0400

    vim surround and incremental selection

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -11,9 +11,14 @@ return {
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
     },
-    indent = { enable = false, disable = { 'ruby' } },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        node_incremental = 'v',
+        node_decremental = 'V',
+      },
+    },
   },
   config = function(_, opts)
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
