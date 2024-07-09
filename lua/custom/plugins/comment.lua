2f4d8d589aa718be1b632f06ccbbbffd2dbfc015
vorhey <jorgeherreraulloa@gmail.com>
2024-07-09 19:29:57 -0400

    small diagnostic comment plugin fix

--- lua/custom/plugins/comment.lua
+++ lua/custom/plugins/comment.lua
@@ -7,6 +7,7 @@ return {
   config = function()
     local comment = require 'Comment'
     local ts_context_commentstring = require 'ts_context_commentstring.integrations.comment_nvim'
+    ---@diagnostic disable-next-line: missing-fields
     comment.setup {
       pre_hook = ts_context_commentstring.create_pre_hook(),
     }
