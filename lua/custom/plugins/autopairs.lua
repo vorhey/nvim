582eb3064939eb93cd37ce20b5ecb4aee8287fc1
vorhey <jorgeherreraulloa@gmail.com>
2024-07-09 17:33:41 -0400

    autopair fix

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -6,7 +6,8 @@ return {
     config_internal_pairs = {
       { '"', '"', suround = false, imap = false, cmap = false }, -- Disabling double quotes pairing
       { "'", "'", suround = false, imap = false, cmap = false }, -- Disabling single quotes pairing
-      { '(', ')', suround = false, imap = false, cmap = false }, -- Disabling single quotes pairing
+      { '(', ')', suround = true, imap = true, cmap = true, disable_start = true },
+      { '{', '}', suround = true, imap = true, cmap = true, disable_start = true },
     },
   },
 }
