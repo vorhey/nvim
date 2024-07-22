2effbe8159766466d4655508e6fbb6a6d369c28d
vorhey <jorgeherreraulloa@gmail.com>
2024-07-22 15:08:30 -0400

    removed animate

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,10 +1,5 @@
 return {
   'echasnovski/mini.nvim',
   version = false,
-  config = function()
-    require('mini.animate').setup {
-      resize = { enable = false },
-      open = { enable = false },
-    }
-  end,
+  config = function() end,
 }
