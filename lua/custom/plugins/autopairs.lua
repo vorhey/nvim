734b5385784903ba785a69295870811ec6ba5110
vorhey <jorgeherreraulloa@gmail.com>
2024-07-27 13:07:14 -0400

    disable exceptions autopairs

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -3,11 +3,11 @@ return {
   event = { 'InsertEnter', 'CmdlineEnter' },
   branch = 'v0.6',
   opts = {
-    config_internal_pairs = {
-      { '"', '"', suround = false, imap = false, cmap = false }, -- Disabling double quotes pairing
-      { "'", "'", suround = false, imap = false, cmap = false }, -- Disabling single quotes pairing
-      { '(', ')', suround = true, imap = true, cmap = true, disable_start = true },
-      { '{', '}', suround = true, imap = true, cmap = true, disable_start = true },
-    },
+    -- config_internal_pairs = {
+    --   { '"', '"', suround = false, imap = false, cmap = false }, -- Disabling double quotes pairing
+    --   { "'", "'", suround = false, imap = false, cmap = false }, -- Disabling single quotes pairing
+    --   { '(', ')', suround = true, imap = true, cmap = true, disable_start = true },
+    --   { '{', '}', suround = true, imap = true, cmap = true, disable_start = true },
+    -- },
   },
 }
