02e1d680e5462d380d1d7db70410b414bc66976a
vorhey <jorgeherreraulloa@gmail.com>
2024-08-10 13:19:36 -0400

    removed mini animate

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -5,13 +5,5 @@ return {
     require('mini.indentscope').setup {
       delay = 250,
     }
-    require('mini.animate').setup {
-      cursor = {
-        timing = require('mini.animate').gen_timing.linear { duration = 5 },
-      },
-      scroll = {
-        timing = require('mini.animate').gen_timing.linear { duration = 5 },
-      },
-    }
   end,
 }
