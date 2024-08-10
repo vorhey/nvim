401b12cc38a0d93ac337111dbd38f4af63a9643c
vorhey <jorgeherreraulloa@gmail.com>
2024-08-09 20:52:26 -0400

    kanagawa theme update

--- lua/custom/themes/kanagawa.lua
+++ lua/custom/themes/kanagawa.lua
@@ -23,12 +23,12 @@ return {
           Pmenu = { bg = 'none' },
           -- Telescope
           TelescopeTitle = { fg = theme.ui.special, bold = true },
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+          TelescopePromptNormal = { bg = 'none' },
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = 'none' },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = 'none' },
+          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = 'none' },
+          TelescopePreviewNormal = { bg = 'none' },
+          TelescopePreviewBorder = { bg = 'none', fg = theme.ui.bg_dim },
           -- Statusbar
           StatusLine = { bg = 'none' },
           StatusLineNC = { bg = 'none' },
