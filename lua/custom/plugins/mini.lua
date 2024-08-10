75b74f3a0df80f65be54291f1dab108143dcdcc6
vorhey <jorgeherreraulloa@gmail.com>
2024-08-09 23:19:11 -0400

    mini indent and mini animations

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,5 +1,17 @@
 return {
   'echasnovski/mini.nvim',
   version = false,
-  config = function() end,
+  config = function()
+    require('mini.indentscope').setup {
+      delay = 250,
+    }
+    require('mini.animate').setup {
+      cursor = {
+        timing = require('mini.animate').gen_timing.linear { duration = 5 },
+      },
+      scroll = {
+        timing = require('mini.animate').gen_timing.linear { duration = 5 },
+      },
+    }
+  end,
 }
