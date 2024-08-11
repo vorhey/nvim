3a5cd09d58901c922add9f5c8e47733794f764b9
vorhey <jorgeherreraulloa@gmail.com>
2024-08-10 20:59:48 -0400

    nvim autopairs

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -1,13 +1,25 @@
 return {
-  'altermo/ultimate-autopair.nvim',
-  event = { 'InsertEnter', 'CmdlineEnter' },
-  branch = 'v0.6',
-  opts = {
-    -- config_internal_pairs = {
-    --   { '"', '"', suround = false, imap = false, cmap = false }, -- Disabling double quotes pairing
-    --   { "'", "'", suround = false, imap = false, cmap = false }, -- Disabling single quotes pairing
-    --   { '(', ')', suround = true, imap = true, cmap = true, disable_start = true },
-    --   { '{', '}', suround = true, imap = true, cmap = true, disable_start = true },
-    -- },
-  },
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+  config = function()
+    require('nvim-autopairs').setup {
+      fast_wrap = {},
+    }
+  end,
 }
+-- return {
+--   'altermo/ultimate-autopair.nvim',
+--   event = { 'InsertEnter', 'CmdlineEnter' },
+--   branch = 'v0.6',
+--   opts = {
+--     -- config_internal_pairs = {
+--     --   { '"', '"', suround = false, imap = false, cmap = false }, -- Disabling double quotes pairing
+--     --   { "'", "'", suround = false, imap = false, cmap = false }, -- Disabling single quotes pairing
+--     --   { '(', ')', suround = true, imap = true, cmap = true, disable_start = true },
+--     --   { '{', '}', suround = true, imap = true, cmap = true, disable_start = true },
+--     -- },
+--     fastwarp = {
+--       enabled = true,
+--     },
+--   },
+-- }
