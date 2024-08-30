3f94fedd63df337be2a3cb2d488575370dd9c443
vorhey <jorgeherreraulloa@gmail.com>
2024-08-29 23:15:24 -0400

    cleaned autopairs

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -7,19 +7,3 @@ return {
     }
   end,
 }
--- return {
---   'altermo/ultimate-autopair.nvim',
---   event = { 'InsertEnter', 'CmdlineEnter' },
---   branch = 'v0.6',
---   opts = {
---     -- config_internal_pairs = {
---     --   { '"', '"', suround = false, imap = false, cmap = false }, -- Disabling double quotes pairing
---     --   { "'", "'", suround = false, imap = false, cmap = false }, -- Disabling single quotes pairing
---     --   { '(', ')', suround = true, imap = true, cmap = true, disable_start = true },
---     --   { '{', '}', suround = true, imap = true, cmap = true, disable_start = true },
---     -- },
---     fastwarp = {
---       enabled = true,
---     },
---   },
--- }
