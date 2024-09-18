60fe5bddf4ba62a1901875f29bcb0f048772ce47
vorhey <jorgeherreraulloa@gmail.com>
2024-09-18 14:39:28 -0300

    gitsigns keys

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -13,17 +13,15 @@ return {
           opts = vim.tbl_extend('force', { noremap = true, silent = true }, opts or {})
           vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
         end
+        local gitsigns = require 'gitsigns'
         local wk = require 'which-key'
         wk.add({
           -- Actions
-          { '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>', desc = 'Stage hunk', mode = { 'n', 'v' } },
-          { '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>', desc = 'Reset hunk', mode = { 'n', 'v' } },
-          { '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>', desc = 'Stage buffer' },
-          { '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>', desc = 'Undo stage hunk' },
-          { '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>', desc = 'Reset buffer' },
-          { '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>', desc = 'Preview hunk' },
-          { '<leader>hb', '<cmd>Gitsigns blame_line{full=true}<CR>', desc = 'Blame line' },
-          { '<leader>hB', '<cmd>Gitsigns toggle_current_line_blame<CR>', desc = 'Toggle line blame' },
+          { '<leader>hr', gitsigns.reset_hunk, desc = 'Reset hunk', mode = { 'n', 'v' } },
+          { '<leader>hR', gitsigns.reset_buffer, desc = 'Restore' },
+          { '<leader>hp', gitsigns.preview_hunk, desc = 'Preview hunk' },
+          { '<leader>hb', gitsigns.blame_line { full = true }, desc = 'Blame line' },
+          { '<leader>hB', gitsigns.toggle_current_line_blame, desc = 'Toggle line blame' },
           { '<leader>hd', "<cmd>lua require('gitsigns').diffthis() vim.api.nvim_command('wincmd h')<CR>", desc = 'Diff view' },
         }, { buffer = bufnr })
       end,
