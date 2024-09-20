0cbf7d804f2bce51cd66eb8f9b005d8c34af0d25
vorhey <jorgeherreraulloa@gmail.com>
2024-09-20 00:33:36 -0300

    updated regex for search word

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -147,9 +147,12 @@ return {
     end
     local grep_string_under_cursor = function()
       local word = vim.fn.expand '<cWORD>'
-      local clean_word = word:match '[%w_-]+'
+      word = word:match 'className="([%w%-_]+)"'
+      if not word then
+        word = word:match '[%w%-_]+'
+      end
       builtin.grep_string {
-        search = clean_word,
+        search = word,
         only_sort_text = true,
         search_dirs = { 'src/', 'lib/' },
         file_ignore_patterns = { '*.min.js', '*.min.css' },
