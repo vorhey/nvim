e960791926b144d31d9fecdfdcc0d1ac2a2bdbb1
vorhey <jorgeherreraulloa@gmail.com>
2024-09-20 00:54:19 -0300

    find word enhanced

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -147,10 +147,9 @@ return {
     end
     local grep_string_under_cursor = function()
       local word = vim.fn.expand '<cWORD>'
-      local search_term = word:match 'className="([%w%-_]+)"'
-      if not search_term then
-        search_term = word:match '[%w%-_]+'
-      end
+      local quoted_text = word:match '["\']([^"\']+)["\']'
+      local cleaned_word = word:match '[%w%-_]+'
+      local search_term = quoted_text or cleaned_word
       builtin.grep_string {
         search = search_term,
         only_sort_text = true,
