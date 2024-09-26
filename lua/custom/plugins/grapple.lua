b8a06ecc3e3b306a574e092b0ad4b0df26a4ed35
vorhey <jorgeherreraulloa@gmail.com>
2024-09-26 12:08:31 -0300

    grapple keybinding switch alt

--- lua/custom/plugins/grapple.lua
+++ lua/custom/plugins/grapple.lua
@@ -11,5 +11,8 @@ return {
     { '<leader>oo', '<cmd>Grapple cycle_tags next<cr>', desc = 'Grapple cycle next tag' },
     { '<leader>op', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Grapple cycle previous tag' },
     { '<leader>od', '<cmd>Grapple reset<cr>', desc = 'Grapple reset' },
+    { '<A-0>', '<cmd>Grapple select index=1<cr>', desc = 'Grapple select tag 1' },
+    { '<A-9>', '<cmd>Grapple select index=2<cr>', desc = 'Grapple select tag 2' },
+    { '<A-8>', '<cmd>Grapple select index=3<cr>', desc = 'Grapple select tag 3' },
   },
 }
