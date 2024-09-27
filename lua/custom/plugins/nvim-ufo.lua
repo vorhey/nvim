871bb2d7ceb0d2b9575f78766b79b62bfc2b961e
vorhey <jorgeherreraulloa@gmail.com>
2024-09-27 09:33:20 -0300

    fix ufo zR zM folding save recalc foldings

--- lua/custom/plugins/nvim-ufo.lua
+++ lua/custom/plugins/nvim-ufo.lua
@@ -9,5 +9,7 @@ return {
         return { 'treesitter', 'indent' }
       end,
     }
+    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
   end,
 }
