557c3a793243173c1d2beb500b57acaab6bf2e19
vorhey <jorgeherreraulloa@gmail.com>
2024-09-27 22:50:43 -0300

    clear supermaven message onstart

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -6,6 +6,10 @@ return {
         accept_suggestion = '<c-y>',
       },
       condition = function()
+        vim.defer_fn(function()
+          vim.cmd 'echo ""'
+          vim.cmd 'redraw'
+        end, 50)
         return true
       end,
     }
