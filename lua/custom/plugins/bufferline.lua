832161e4b4b04fc18e08f798886ff1d0d3bf9d01
vorhey <jorgeherreraulloa@gmail.com>
2024-10-22 23:58:58 -0300

    bufferline keybindings

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -8,5 +8,10 @@ return {
         style_preset = bufferline.style_preset.no_italic,
       },
     }
+    -- Bufferline switching keymaps
+    vim.keymap.set('n', '<A-q>', '<cmd>BufferLineGoToBuffer 1<CR>', { desc = 'Switch to buffer 1', silent = true })
+    vim.keymap.set('n', '<A-w>', '<cmd>BufferLineGoToBuffer 2<CR>', { desc = 'Switch to buffer 2', silent = true })
+    vim.keymap.set('n', '<A-e>', '<cmd>BufferLineGoToBuffer 3<CR>', { desc = 'Switch to buffer 3', silent = true })
+    vim.keymap.set('n', '<A-r>', '<cmd>BufferLineGoToBuffer 4<CR>', { desc = 'Switch to buffer 4', silent = true })
   end,
 }
