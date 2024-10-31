a1baf43f09cf20b5408e6648b23c4b966855e2f1
vorhey <jorgeherreraulloa@gmail.com>
2024-10-31 03:17:48 -0300

    supermaven start fix

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -1,17 +1,11 @@
 return {
   'supermaven-inc/supermaven-nvim',
+  cmd = { 'SupermavenStart' },
   config = function()
     require('supermaven-nvim').setup {
       keymaps = {
         accept_suggestion = '<c-l>',
       },
-      condition = function()
-        vim.defer_fn(function()
-          vim.cmd 'echo ""'
-          vim.cmd 'redraw'
-        end, 50)
-        return true
-      end,
     }
   end,
 }
