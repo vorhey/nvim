5eae5473f1b45d96a8aa47e3d1ad108a40993aa7
vorhey <jorgeherreraulloa@gmail.com>
2024-11-08 16:31:38 -0300

    updated telescope branch

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
   -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
-  tag = '0.1.6',
+  tag = '0.1.8',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for installation instructions
