2059f2c4d65b8bb07668054e7a17f132a42554a0
vorhey <jorgeherreraulloa@gmail.com>
2024-11-26 11:31:18 -0300

    top right corner signature

--- lua/custom/plugins/lsp-signature.lua
+++ lua/custom/plugins/lsp-signature.lua
@@ -1,14 +1,37 @@
 return {
   'ray-x/lsp_signature.nvim',
   event = 'VeryLazy',
-  opts = {
-    doc_lines = 0,
-    hint_enable = false,
-    floating_window = true,
-    toggle_key = '<M-k>',
-    toggle_key_flip_floatwin_setting = true,
-  },
-  config = function(_, opts)
-    require('lsp_signature').setup(opts)
+  config = function()
+    local function get_win_dim()
+      return {
+        width = vim.api.nvim_win_get_width(0),
+        height = vim.api.nvim_win_get_height(0),
+      }
+    end
+    local cfg = {
+      floating_window_off_x = function()
+        return get_win_dim().width
+      end,
+
+      floating_window_off_y = function()
+        return -get_win_dim().height
+      end,
+
+      -- Ensure window appears above cursor line
+      floating_window_above_cur_line = true,
+
+      -- You might want to adjust these related settings:
+      max_width = 80, -- adjust based on your needs
+      max_height = 12,
+      fix_pos = true, -- set to true to maintain position
+
+      doc_lines = 0,
+      hint_enable = false,
+      floating_window = true,
+      toggle_key = '<M-k>',
+      toggle_key_flip_floatwin_setting = true,
+    }
+
+    require('lsp_signature').setup(cfg)
   end,
 }
