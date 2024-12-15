3fda25c1852343a3d2ee1adf62af8c7b632bc4e8
vorhey <jorgeherreraulloa@gmail.com>
2024-12-15 16:01:39 -0300

    updated lualine again

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,28 +1,22 @@
 return {
   'nvim-lualine/lualine.nvim',
   event = 'VeryLazy',
-  enabled = false,
+  enabled = true,
   config = function()
-    local utils = require 'utils'
-    local custom_theme = require 'lualine.themes.auto'
-    -- Function to recursively set background to 'none'
-    local function set_bg_none(tbl)
-      for k, v in pairs(tbl) do
-        if type(v) == 'table' then
-          if k ~= 'a' and v.bg then
-            v.bg = 'none'
-          end
-          set_bg_none(v)
-        end
-      end
-    end
-    -- Line endings
+    local M = {}
+
+    local mode_width = 0
+    local branch_width = 0
+    local diff_width = 0
+    local filetype_width = 0
+    local filename_width = 0
+
     local function line_ending()
       local format = vim.bo.fileformat
       local symbols = { unix = 'LF', dos = 'CRLF', mac = 'CR' }
       return symbols[format] or format
     end
-    -- Spacing info
+
     local function spacing_info()
       if vim.bo.expandtab then
         return 'Spaces: ' .. vim.bo.shiftwidth
@@ -30,134 +24,192 @@ return {
         return 'Tabs: ' .. vim.bo.tabstop
       end
     end
-    -- Buffers
-    local function buffer_list()
-      local output = {}
-      local buffers = vim.fn.getbufinfo { buflisted = 1 }
-      local current = vim.fn.bufnr '%'
-      for i, buf in ipairs(buffers) do
-        if buf.bufnr == current then
-          table.insert(output, string.format('[%d]', i))
-        else
-          table.insert(output, tostring(i))
-        end
-      end
-      return table.concat(output, ' ')
-    end
-    -- Function signature
-    local current_signature = function(width)
-      local sig = require('lsp_signature').status_line(width)
-      if not sig.label or sig.label == '' then
-        return ''
-      end
 
-      -- Find the position of the first '('
-      local start_pos = sig.label:find '%('
-      if start_pos then
-        -- Return everything from '(' onwards
-        return '󰊕' .. sig.label:sub(start_pos)
-      else
-        return ''
-      end
-    end
-    -- Search custom
-    local search_custom = function()
-      local search = vim.fn.searchcount()
-      if search.total > 0 and vim.v.hlsearch == 1 then
-        return ' ' .. search.current .. '/' .. search.total
-      else
-        return ''
-      end
-    end
-    -- Apply transparent background to all theme sections
-    set_bg_none(custom_theme)
+    M.sections = {
+      lualine_a = {
+        {},
+      },
+      lualine_b = {
+        {
+          'branch',
+          cond = function()
+            local should_show = vim.opt.columns:get() > 60
+            if not should_show then
+              branch_width = 0
+            end
+            return should_show
+          end,
+          fmt = function(str)
+            if str == '' then
+              branch_width = 0
+              return ''
+            end
+            branch_width = #str + 2 + 2
+            return str
+          end,
+        },
+        {
+          'diff',
+          cond = function()
+            local should_show = vim.opt.columns:get() > 60
+            if not should_show then
+              diff_width = 0
+            end
+            return should_show
+          end,
+          fmt = function(str)
+            if str == '' then
+              diff_width = 0
+              return ''
+            end
+            local evaled_str = vim.api.nvim_eval_statusline(str, {}).str
+            diff_width = #evaled_str + 2
+            return str
+          end,
+        },
+      },
+      lualine_c = {
+        {
+          function()
+            local used_space = mode_width + branch_width + diff_width
+            local win_width = vim.opt.columns:get()
+            local fill_space = string.rep(' ', math.floor((win_width - filename_width - filetype_width) / 2) - used_space)
+            return fill_space
+          end,
+          padding = { left = 0, right = 0 },
+          cond = function()
+            return vim.opt.columns:get() > 60
+          end,
+        },
+        {
+          'filetype',
+          fmt = function(str)
+            if str == '' then
+              filetype_width = 0
+              return ''
+            end
+            filetype_width = 1 + 2
+            return str
+          end,
+          icon_only = true,
+        },
+        {
+          'filename',
+          fmt = function(filename)
+            if filename == '' then
+              filename_width = 0
+              return ''
+            end
+
+            local used_space = mode_width + branch_width + diff_width
+            local win_width = vim.opt.columns:get()
+            local free_space = (math.floor(win_width / 2) - used_space) * 2
+
+            if free_space < #filename + filetype_width + 10 then
+              filename = vim.fn.expand '%:t'
+            end
+
+            if free_space < #filename + filetype_width + 10 then
+              local end_idx = free_space - filetype_width - 13
+              if end_idx < 0 then
+                return ''
+              end
+              filename = string.sub(filename, 1, end_idx) .. '...'
+            end
+
+            filename_width = #filename + 2
+
+            return filename
+          end,
+          file_status = true,
+          newfile_status = false,
+          path = 0,
+          shorting_target = 0,
+          symbols = {
+            modified = '[+]',
+            readonly = '[-]',
+            unnamed = '[No Name]',
+            newfile = '[New]',
+          },
+        },
+      },
+      lualine_x = {
+        { 'diagnostics' },
+      },
+      lualine_y = {
+        {
+          'encoding',
+          cond = function()
+            return vim.opt.columns:get() > 80
+          end,
+        },
+      },
+      lualine_z = {
+        {
+          line_ending,
+          padding = { left = 1, right = 0 },
+        },
+        spacing_info,
+      },
+    }
+
     require('lualine').setup {
       options = {
-        theme = custom_theme,
-        globalstatus = false,
-        disabled_filetypes = {
-          statusline = { 'NvimTree', 'alpha', 'dap-repl', 'dapui_console', 'dapui_watches', 'dapui_stacks', 'dapui_breakpoints', 'dapui_scopes' },
-        },
-        section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
-      },
-      sections = {
-        lualine_a = {},
-        lualine_b = {
-          {
-            'branch',
-            separator = { left = '', right = '' },
-            color = utils.get_hlgroup 'Comment',
+        section_separators = { left = '', right = '' },
+        globalstatus = false,
+        theme = {
+          normal = {
+            a = { bg = 'NONE' },
+            b = { bg = 'NONE' },
+            c = { bg = 'NONE' },
+            x = { bg = 'NONE' },
+            y = { bg = 'NONE' },
+            z = { bg = 'NONE' },
           },
-          {
-            'grapple',
-            draw_empty = true,
-            separator = { left = '', right = '' },
+          insert = {
+            a = { bg = 'NONE' },
+            b = { bg = 'NONE' },
+            c = { bg = 'NONE' },
+            x = { bg = 'NONE' },
+            y = { bg = 'NONE' },
+            z = { bg = 'NONE' },
           },
-        },
-        lualine_c = {
-          '%=',
-          {
-            'filename',
-            path = 0,
-            shorting_target = 20,
-            separator = { left = '', right = '' },
+          visual = {
+            a = { bg = 'NONE' },
+            b = { bg = 'NONE' },
+            c = { bg = 'NONE' },
+            x = { bg = 'NONE' },
+            y = { bg = 'NONE' },
+            z = { bg = 'NONE' },
           },
-          {
-            'filetype',
-            colored = true,
-            icon_only = true,
-            separator = { left = '', right = '' },
+          replace = {
+            a = { bg = 'NONE' },
+            b = { bg = 'NONE' },
+            c = { bg = 'NONE' },
+            x = { bg = 'NONE' },
+            y = { bg = 'NONE' },
+            z = { bg = 'NONE' },
           },
-        },
-        lualine_x = {},
-        lualine_y = {
-          {
-            search_custom,
-            separator = { left = '', right = '' },
-            color = { fg = utils.get_hlgroup('Function').fg },
+          command = {
+            a = { bg = 'NONE' },
+            b = { bg = 'NONE' },
+            c = { bg = 'NONE' },
+            x = { bg = 'NONE' },
+            y = { bg = 'NONE' },
+            z = { bg = 'NONE' },
           },
-          {
-            'diagnostics',
-            symbols = {
-              error = ' ',
-              warn = ' ',
-              hint = ' ',
-              info = ' ',
-            },
-            diagnostics_color = {
-              error = 'Comment', -- Changes diagnostics' error color.
-              warn = 'Comment', -- Changes diagnostics' warn color.
-              info = 'Comment', -- Changes diagnostics' info color.
-              hint = 'Comment', -- Changes diagnostics' hint color.
-            },
-            separator = { left = '', right = '' },
-          },
-          {
-            require('lazy.status').updates,
-            cond = require('lazy.status').has_updates,
-            separator = { left = '', right = '' },
-            color = utils.get_hlgroup 'Comment',
-          },
-        },
-        lualine_z = {
-          {
-            line_ending,
-            separator = { left = '', right = '' },
-            color = utils.get_hlgroup 'Comment',
-          },
-          {
-            spacing_info,
-            separator = { left = '', right = '' },
-            color = utils.get_hlgroup 'Comment',
-          },
-          {
-            'encoding',
-            color = utils.get_hlgroup 'Comment',
+          inactive = {
+            a = { bg = 'NONE' },
+            b = { bg = 'NONE' },
+            c = { bg = 'NONE' },
+            x = { bg = 'NONE' },
+            y = { bg = 'NONE' },
+            z = { bg = 'NONE' },
           },
         },
       },
-      extensions = { 'lazy', 'toggleterm', 'mason' },
+      sections = M.sections,
     }
   end,
 }
