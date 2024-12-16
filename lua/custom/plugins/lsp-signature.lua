e28bc6fbdcb2d3b3060780362d96ac47f244d879
vorhey <jorgeherreraulloa@gmail.com>
2024-12-15 22:51:03 -0300

    updated lsp signature

--- lua/custom/plugins/lsp-signature.lua
+++ lua/custom/plugins/lsp-signature.lua
@@ -8,6 +8,7 @@ return {
         height = vim.api.nvim_win_get_height(0),
       }
     end
+
     local cfg = {
       floating_window_off_x = function()
         return get_win_dim().width
@@ -18,14 +19,18 @@ return {
       end,
 
       doc_lines = 0,
+      wrap = true,
       floating_window = true,
       floating_window_above_cur_line = true,
       fix_pos = true,
       hint_enable = false,
-      max_width = 80,
-      max_height = 12,
+      max_width = 60,
+      max_height = 80,
       toggle_key = '<M-k>',
       toggle_key_flip_floatwin_setting = true,
+      handler_opts = {
+        border = 'rounded',
+      },
     }
 
     require('lsp_signature').setup(cfg)
