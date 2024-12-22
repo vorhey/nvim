03af1b868e85ae72ef7622f25ca299a707d0f958
vorhey <jorgeherreraulloa@gmail.com>
2024-12-22 05:07:58 -0300

    updated signature

--- lua/custom/plugins/lsp-signature.lua
+++ lua/custom/plugins/lsp-signature.lua
@@ -4,11 +4,13 @@ return {
   config = function()
     local cfg = {
       floating_window = false,
-      hint_inline = function()
-        return true
-      end,
-      hint_prefix = '',
       hint_scheme = 'Comment',
+      hint_enable = true,
+      hint_prefix = {
+        above = '↙ ', -- when the hint is on the line above the current line
+        current = '← ', -- when the hint is on the same line
+        below = '↖ ', -- when the hint is on the line below the current line
+      },
     }
 
     require('lsp_signature').setup(cfg)
