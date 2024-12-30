de04d21f3399c648ac589a69efd9861b37716e35
vorhey <jorgeherreraulloa@gmail.com>
2024-12-30 19:33:12 -0300

    fzf lua grep alt

--- lua/custom/plugins/fzf-lua.lua
+++ lua/custom/plugins/fzf-lua.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     require('fzf-lua').setup {
       vim.keymap.set('n', '<c-P>', require('fzf-lua').files, { desc = 'Fzf Files' }),
+      vim.keymap.set('n', '<leader>fG', require('fzf-lua').grep, { desc = 'Find: Fzf Grep' }),
     }
   end,
 }
