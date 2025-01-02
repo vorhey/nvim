f5c7ecf5c7c8eaf2801da7ebbe5bc9b600635224
vorhey <jorgeherreraulloa@gmail.com>
2025-01-02 06:05:03 -0300

    removed clutter and semantic tokens

--- lua/utils.lua
+++ lua/utils.lua
@@ -133,71 +133,8 @@ M.copy_line_above = function()
   end
 end
 
--- Semantic tokens
-M.semantick_tokens = function(client)
-  -- NOTE: Super hacky... Don't know if I like that we set a random variable on the client
-  -- Seems to work though
-  if client.is_hacked then
-    return
-  end
-  client.is_hacked = true
-
-  -- let the runtime know the server can do semanticTokens/full now
-  client.server_capabilities = vim.tbl_deep_extend('force', client.server_capabilities, {
-    semanticTokensProvider = {
-      full = true,
-    },
-  })
-
-  -- monkey patch the request proxy
-  local request_inner = client.request
-  client.request = function(method, params, handler, req_bufnr)
-    if method ~= vim.lsp.protocol.Methods.textDocument_semanticTokens_full then
-      return request_inner(method, params, handler)
-    end
-
-    local target_bufnr = vim.uri_to_bufnr(params.textDocument.uri)
-    local line_count = vim.api.nvim_buf_line_count(target_bufnr)
-    local last_line = vim.api.nvim_buf_get_lines(target_bufnr, line_count - 1, line_count, true)[1]
-
-    return request_inner('textDocument/semanticTokens/range', {
-      textDocument = params.textDocument,
-      range = {
-        ['start'] = {
-          line = 0,
-          character = 0,
-        },
-        ['end'] = {
-          line = line_count - 1,
-          character = string.len(last_line) - 1,
-        },
-      },
-    }, handler, req_bufnr)
-  end
-end
-
 M.is_dotnet_installed = function()
   return vim.fn.executable 'dotnet' == 1
-end
-
-M.pick_file = function(glob_pattern, ignore_patterns)
-  return coroutine.create(function(dap_run_co)
-    require('telescope.builtin').find_files {
-      cwd = vim.fn.getcwd(),
-      file_ignore_patterns = ignore_patterns or {},
-      find_command = { 'rg', '--files', '--glob', glob_pattern },
-      respect_gitignore = false,
-      previewer = false,
-      attach_mappings = function(_, map)
-        map('i', '<CR>', function(prompt_bufnr)
-          local selection = require('telescope.actions.state').get_selected_entry()
-          require('telescope.actions').close(prompt_bufnr)
-          coroutine.resume(dap_run_co, selection.value)
-        end)
-        return true
-      end,
-    }
-  end)
 end
 
 return M
