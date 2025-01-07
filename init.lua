b971b261f07885211bf310c915d96b12f7297a01
Jorge Herrera Ulloa <jorgeherreraulloa@gmail.com>
2025-01-07 15:16:14 -0300

    updated init lua

--- init.lua
+++ init.lua
@@ -4,9 +4,12 @@ require 'keybindings' -- Load key mappings
 require 'autocmds' -- Load autocommands
 -- Plugin management with `lazy.nvim`
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
