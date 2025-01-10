c003cb2e7c92af9c8c8b29a88481b98e1b5fcaf5
vorhey <jorgeherreraulloa@gmail.com>
2025-01-10 01:08:14 -0300

    updated surround key

--- lua/custom/plugins/nvim-surround.lua
+++ lua/custom/plugins/nvim-surround.lua
@@ -3,6 +3,13 @@ return {
   version = '*', -- Use for stability; omit to use `main` branch for the latest features
   event = 'VeryLazy',
   config = function()
-    require('nvim-surround').setup {}
+    require('nvim-surround').setup {
+      keymaps = {
+        normal = 'gs',
+        visual = 'S',
+        delete = 'ds',
+        change = 'cs',
+      },
+    }
   end,
 }
