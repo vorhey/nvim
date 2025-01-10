682b47ac75f2367f87d28ba614e4305a1c19e232
vorhey <jorgeherreraulloa@gmail.com>
2025-01-10 01:56:39 -0300

    auto-pairs lua function

--- lua/custom/plugins/nvim-autopairs.lua
+++ lua/custom/plugins/nvim-autopairs.lua
@@ -3,8 +3,17 @@ return {
   event = 'InsertEnter',
   config = function()
     local npairs = require 'nvim-autopairs'
+    local Rule = require 'nvim-autopairs.rule'
     npairs.setup {
       ignored_next_char = string.gsub([[ [%w%.%(%{%[%$'"`%s] ]], '%s+', ''),
+    }
+    npairs.add_rules {
+      Rule('function%(.*%)%s*$', 'end', 'lua')
+        :use_regex(true)
+        :set_end_pair_length(3) -- end is 3 characters
+        :replace_endpair(function()
+          return '\n\t\nend' -- add newlines and tab
+        end),
     }
   end,
 }
