875695d8bbe773e0b5564f4a26b4c3c83861c44e
vorhey <jorgeherreraulloa@gmail.com>
2025-01-18 11:40:56 -0300

    blink cmp display docs

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -16,6 +16,12 @@ return {
     sources = {
       default = { 'lsp', 'path' },
     },
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+    },
   },
   opts_extend = { 'sources.default' },
 }
