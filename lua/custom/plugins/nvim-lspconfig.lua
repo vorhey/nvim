afb0ae279775ed1fab0cd1bb988a9f29f5aba173
Jorge Herrera Ulloa <jorgeherreraulloa@gmail.com>
2025-01-19 22:54:34 -0300

    gopls extended settings

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -135,10 +135,27 @@ return {
         settings = {
           gopls = {
             gofumpt = true,
-            analyses = {
-              unusedparams = true,
+            codelenses = {
+              gc_details = false,
+              generate = true,
+              regenerate_cgo = true,
+              run_govulncheck = true,
+              test = true,
+              tidy = true,
+              upgrade_dependency = true,
+              vendor = true,
             },
+            analyses = {
+              nilness = true,
+              unusedparams = true,
+              unusedwrite = true,
+              useany = true,
+            },
+            usePlaceholders = true,
+            completeUnimported = true,
             staticcheck = true,
+            directoryFilters = { '-.git', '-.vscode', '-.idea', '-.vscode-test', '-node_modules' },
+            semanticTokens = true,
           },
         },
       },
