ad58b5c14620e4c92da433d32a8e8b25d511ae65
vorhey <jorgeherreraulloa@gmail.com>
2025-01-19 22:17:31 -0300

    add border to cmp

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -20,6 +20,12 @@ return {
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 200,
+        window = {
+          border = 'rounded',
+        },
+      },
+      menu = {
+        border = 'rounded',
       },
     },
   },
