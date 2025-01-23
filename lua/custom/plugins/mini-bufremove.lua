aa7eff35496580ba04bebeb6d591da22ceaf079e
Jorge Herrera Ulloa <jorgeherreraulloa@gmail.com>
2025-01-23 15:44:25 -0300

    removed q keybinding

--- lua/custom/plugins/mini-bufremove.lua
+++ lua/custom/plugins/mini-bufremove.lua
@@ -2,18 +2,6 @@ return {
   'echasnovski/mini.bufremove',
   version = false,
   config = function()
-    vim.keymap.set('n', 'q', function()
-      require('mini.bufremove').delete(0, false) -- 0 means current buffer, false means don't force delete
-    end, { desc = 'Buffer: Close' })
-    vim.keymap.set('n', '<leader>o', function()
-      local current = vim.api.nvim_get_current_buf()
-      local buffers = vim.api.nvim_list_bufs()
-      for _, buf in ipairs(buffers) do
-        if buf ~= current and vim.api.nvim_buf_is_loaded(buf) then
-          require('mini.bufremove').delete(buf, false)
-        end
-      end
-    end, { desc = 'Close all other buffers' })
     require('mini.bufremove').setup {}
   end,
 }
