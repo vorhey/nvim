89c71be289c6e97f6e5bf421c8de8278e7a1597d
vorhey <jorgeherreraulloa@gmail.com>
2025-01-25 19:48:26 -0300

    add avante

---
+++ lua/custom/plugins/avante.lua
@@ -0,0 +1,28 @@
+return {
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
+  lazy = false,
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  opts = {
+    provider = 'gemini',
+    gemini = {
+      model = 'gemini-2.0-flash-exp',
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = 'make',
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    'stevearc/dressing.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
+      },
+      ft = { 'markdown', 'Avante' },
+    },
+  },
+}
