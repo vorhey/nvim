d06ced70c9a73265e33c6ac8d96c0ce4930d2d32
vorhey <jorgeherreraulloa@gmail.com>
2025-01-30 16:23:51 -0300

    updated ultimate-autopairs behavior

--- lua/custom/plugins/ultimate-autopairs.lua
+++ lua/custom/plugins/ultimate-autopairs.lua
@@ -2,5 +2,14 @@ return {
   'altermo/ultimate-autopair.nvim',
   event = { 'InsertEnter', 'CmdlineEnter' },
   branch = 'v0.6',
-  opts = {},
+  opts = {
+    internal_pairs = {
+      { '[', ']', alpha_after = true, newline = true },
+      { '(', ')', alpha_after = true, newline = true },
+      { '{', '}', alpha_after = true, newline = true },
+      { '"', '"', alpha_after = true },
+      { "'", "'", alpha_after = true },
+      { '`', '`', alpha_after = true },
+    },
+  },
 }
