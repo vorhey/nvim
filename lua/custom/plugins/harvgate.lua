6b3ac4b4ee903b550eb6dd0fb8971a5116b44c11
vorhey <jorgeherreraulloa@gmail.com>
2025-01-29 22:38:52 -0300

    reinstated harvgate

---
+++ lua/custom/plugins/harvgate.lua
@@ -0,0 +1,11 @@
+return {
+  'vorhey/harvgate',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('harvgate').setup {}
+    vim.keymap.set('n', '<leader>a', ':HarvgateChat<CR>', { desc = 'Harvgate' })
+  end,
+}
