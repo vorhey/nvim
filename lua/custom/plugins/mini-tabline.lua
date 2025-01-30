7fff8964a8bb0ac1f162aeb959fb306d81763014
jorge <jorge@CL-IT02528.>
2025-01-30 04:21:26 -0300

    mini tabline separator

--- lua/custom/plugins/mini-tabline.lua
+++ lua/custom/plugins/mini-tabline.lua
@@ -2,7 +2,16 @@ return {
   'echasnovski/mini.tabline',
   version = false,
   config = function()
-    require('mini.tabline').setup {}
+    require('mini.tabline').setup {
+      format = function(buf_id, label)
+        local separator = ' │ ' -- Customize the separator
+        local formatted_label = MiniTabline.default_format(buf_id, label) -- Preserve the default formatting
+        if buf_id == vim.fn.bufnr '$' then
+          separator = '' -- Avoid trailing separator
+        end
+        return formatted_label .. separator
+      end,
+    }
     local function get_nth_buffer(n)
       local buffers = vim.tbl_filter(function(buf)
         return vim.bo[buf].buflisted
