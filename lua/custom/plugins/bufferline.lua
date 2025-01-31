a49cfb25c4fbb2d64098fcc4c02a446a3854782e
Jorge Herrera Ulloa <jorgeherreraulloa@gmail.com>
2025-01-31 20:45:10 -0300

    bufferline reordering

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -13,7 +13,9 @@ return {
     vim.keymap.set('n', '<A-w>', '<cmd>BufferLineGoToBuffer 2<CR>', { desc = 'Switch to buffer 2', silent = true })
     vim.keymap.set('n', '<A-e>', '<cmd>BufferLineGoToBuffer 3<CR>', { desc = 'Switch to buffer 3', silent = true })
     vim.keymap.set('n', '<A-r>', '<cmd>BufferLineGoToBuffer 4<CR>', { desc = 'Switch to buffer 4', silent = true })
-    vim.keymap.set('n', '<A-Left>', '<cmd>BufferLineMovePrev<CR>', { desc = 'Move buffer left', silent = true })
-    vim.keymap.set('n', '<A-Right>', '<cmd>BufferLineMoveNext<CR>', { desc = 'Move buffer right', silent = true })
+    vim.keymap.set('n', '<M-h>', '<cmd>BufferLineMovePrev<CR>', { desc = 'Move buffer left', silent = true })
+    vim.keymap.set('n', '<M-l>', '<cmd>BufferLineMoveNext<CR>', { desc = 'Move buffer right', silent = true })
+    vim.keymap.set('n', '[b', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Cycle to prev buffer', silent = true })
+    vim.keymap.set('n', ']b', '<cmd>BufferLineCycleNext<CR>', { desc = 'Cycle to next buffer', silent = true })
   end,
 }
