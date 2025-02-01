4f7befa2ce3ba5b5cad65c8f9fabd852579ff755
vorhey <jorgeherreraulloa@gmail.com>
2025-01-31 23:28:26 -0300

    fix bufferline keybindings

--- lua/custom/plugins/mini-move.lua
+++ lua/custom/plugins/mini-move.lua
@@ -2,6 +2,13 @@ return {
   'echasnovski/mini.move',
   version = false,
   config = function()
-    require('mini.move').setup {}
+    require('mini.move').setup {
+      mappings = {
+        left = '',
+        right = '',
+        line_left = '',
+        line_right = '',
+      },
+    }
   end,
 }
