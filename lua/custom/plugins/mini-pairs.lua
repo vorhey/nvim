41985d8e80343b1e738aef96ac03abec341cf2b2
Jorge Herrera Ulloa <jorgeherreraulloa@gmail.com>
2025-01-31 23:35:44 -0300

    mini pairs updated algo

--- lua/custom/plugins/mini-pairs.lua
+++ lua/custom/plugins/mini-pairs.lua
@@ -18,12 +18,19 @@ return {
       local cursor = vim.api.nvim_win_get_cursor(0)
       local next = line:sub(cursor[2] + 1, cursor[2] + 1)
       local before = line:sub(1, cursor[2])
+
+      if next ~= '' and next:match '%S' then
+        return o
+      end
+
       if opts.markdown and o == '`' and vim.bo.filetype == 'markdown' and before:match '^%s*``' then
         return '`\n```' .. vim.api.nvim_replace_termcodes('<up>', true, true, true)
       end
+
       if opts.skip_next and next ~= '' and next:match(opts.skip_next) then
         return o
       end
+
       if opts.skip_unbalanced and next == c and c ~= o then
         local _, count_open = line:gsub(vim.pesc(pair:sub(1, 1)), '')
         local _, count_close = line:gsub(vim.pesc(pair:sub(2, 2)), '')
@@ -31,6 +38,7 @@ return {
           return o
         end
       end
+
       return open(pair, neigh_pattern)
     end
   end,
