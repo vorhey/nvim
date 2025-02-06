f545f39bf1857178ebcf1ef85a26319e18956d78
jorge <jorge@CL-IT02528.>
2025-02-06 18:22:53 -0300

    ultimate-autopairs removed pair quotes

--- lua/custom/plugins/ultimate-autopair.lua
+++ lua/custom/plugins/ultimate-autopair.lua
@@ -1,6 +1,11 @@
-
 return {
   'altermo/ultimate-autopair.nvim',
   event = { 'InsertEnter', 'CmdlineEnter' },
-  opts = {},
+  opts = {
+    internal_pairs = {
+      { '[', ']', fly = true, dosuround = true, newline = true, space = true },
+      { '(', ')', fly = true, dosuround = true, newline = true, space = true },
+      { '{', '}', fly = true, dosuround = true, newline = true, space = true },
+    },
+  },
 }
