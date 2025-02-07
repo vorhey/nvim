4feb62b784c33601cee3025d3382aa24b1007be2
vorhey <jorgeherreraulloa@gmail.com>
2025-02-07 00:29:32 -0300

    gitsign numhl

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -2,6 +2,7 @@ return {
   'lewis6991/gitsigns.nvim',
   config = function()
     require('gitsigns').setup {
+      numhl = true,
       signs = {
         add = { text = '▎' },
         change = { text = '▎' },
