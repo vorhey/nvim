197c11ffc7c6b2554844cfe1e2ebcfe6dd9e68d1
jorge <jorge@CL-IT02528.>
2025-02-09 15:42:16 -0300

    updated replace key binding

--- lua/custom/plugins/grug-far.lua
+++ lua/custom/plugins/grug-far.lua
@@ -6,12 +6,12 @@ return {
       -- there are no required options atm
       -- engine = 'ripgrep' is default, but 'astgrep' can be specified
     }
-    vim.keymap.set('n', '<c-h>', function()
+    vim.keymap.set('n', '<leader>r', function()
       require('grug-far').open {
         prefills = {
           paths = vim.fn.expand '%',
         },
       }
-    end, { desc = 'replace (file)' })
+    end, { desc = 'replace' })
   end,
 }
