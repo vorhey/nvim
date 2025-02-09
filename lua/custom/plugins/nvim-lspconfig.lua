d27bdf0beaea5e364e8638325abb3b5b95bd3cd5
vorhey <jorgeherreraulloa@gmail.com>
2025-02-09 19:23:33 -0300

    add nvim-ufo

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -23,6 +23,10 @@ return {
     -- Capabilities
     local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
     capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
 
     -- Handlers
     local handlers = {
