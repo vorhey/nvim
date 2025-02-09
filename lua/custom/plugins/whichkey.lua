c98f91bfa893319fd1f2a525ca3a71481678e8af
vorhey <jorgeherreraulloa@gmail.com>
2025-02-09 13:17:13 -0300

    restored whichkey

---
+++ lua/custom/plugins/whichkey.lua
@@ -0,0 +1,30 @@
+return {
+  -- Useful plugin to show you pending keybinds.
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    local wk = require 'which-key'
+    wk.add {
+      mode = { 'n' },
+      {
+        { '<leader>o', icon = { icon = '󰅙', color = 'orange' } },
+        { '<leader>O', icon = { icon = '󰅙', color = 'orange' } },
+        { '<leader>q', icon = { icon = '󰈆', color = 'orange' } },
+        { '<leader>Q', icon = { icon = '󰈆', color = 'orange' } },
+        { '<leader>e', icon = { icon = '󱏒', color = 'orange' } },
+        { '<leader><leader>', icon = { icon = '', color = 'orange' } },
+        { '<leader>a', group = 'harvgate', icon = { icon = '󰁤', color = 'grey' } },
+        { '<leader>d', group = 'debug', icon = { icon = '', color = 'grey' } },
+        { '<leader>t', group = 'features', icon = { icon = '', color = 'grey' } },
+        { '<leader>f', group = 'find', icon = { icon = '', color = 'grey' } },
+        { '<leader>h', group = 'git', icon = { icon = '󰊢', color = 'grey' } },
+        { '<leader>l', group = 'lsp', icon = { icon = '', color = 'grey' } },
+        { '<leader>m', group = 'testing', icon = { icon = '', color = 'grey' } },
+      },
+    }
+    wk.setup {
+      preset = 'helix',
+      sort = { 'manual' },
+    }
+  end,
+}
