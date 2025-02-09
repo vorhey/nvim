b876f41ce01441f7c38e148f734d939cadaa4ca5
vorhey <jorgeherreraulloa@gmail.com>
2025-02-09 14:20:10 -0300

    updated search and replace

--- lua/settings.lua
+++ lua/settings.lua
@@ -147,3 +147,5 @@ vim.cmd [[let &t_Cs = "\e[4:3m"]]
 vim.cmd [[let &t_Ce = "\e[4:0m"]]
 
 vim.opt.winbar = '%=' .. utils.get_root_dirname() .. "%{%v:lua.vim.fn.expand('%')%}%="
+
+vim.opt.inccommand = 'nosplit'
