49c6c05d4e9924f9edcdaaf5ebb032b70f4a5399
vorhey <jorgeherreraulloa@gmail.com>
2025-02-13 09:09:28 -0300

    snacks updated layout

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -6,6 +6,22 @@ return {
       -- your picker configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
+      layout = {
+        layout = {
+          box = 'horizontal',
+          width = 0.8,
+          min_width = 120,
+          height = 0.8,
+          {
+            box = 'vertical',
+            border = 'rounded',
+            title = '{title} {live} {flags}',
+            { win = 'input', height = 1, border = 'bottom' },
+            { win = 'list', border = 'none' },
+          },
+          { win = 'preview', title = '{preview}', border = 'rounded', width = 0.2 },
+        },
+      },
       win = {
         input = {
           keys = {
