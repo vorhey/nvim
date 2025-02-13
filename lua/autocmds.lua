ffa62b3588d1394f2e38ba3d807c963591ef97a5
vorhey <jorgeherreraulloa@gmail.com>
2025-02-13 20:05:18 -0300

    tiny glimmer

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -7,14 +7,6 @@
 
 local general = vim.api.nvim_create_augroup('General', { clear = true })
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank-group', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
 vim.api.nvim_create_autocmd('Bufenter', {
   callback = function()
     vim.opt.formatoptions:remove { 'c', 'r', 'o' }
---
+++ lua/custom/plugins/tiny-glimmer.lua
@@ -0,0 +1,20 @@
+return {
+  'rachartier/tiny-glimmer.nvim',
+  event = 'VeryLazy',
+  opts = {
+    overwrite = {
+      search = {
+        enabled = true,
+      },
+      paste = {
+        enabled = true,
+      },
+      undo = {
+        enabled = true,
+      },
+      redo = {
+        enabled = true,
+      },
+    },
+  },
+}
