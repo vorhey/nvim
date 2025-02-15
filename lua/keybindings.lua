8218cba4e399e8e8d1e3252bf9aa9df702c77b44
vorhey <jorgeherreraulloa@gmail.com>
2025-02-15 09:20:45 -0300

    fterm toggle

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -31,10 +31,6 @@ vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>l', { desc = 'Save file', silent = true
 -- nvimtree
 vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'explorer' })
 
--- fterm
-vim.keymap.set('n', '<c-t>', '<CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set('t', '<c-t>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
-
 -- center screen
 vim.keymap.set('n', '<c-d>', utils.lazy '<c-d>zz', { desc = 'Scroll down half screen' })
 
