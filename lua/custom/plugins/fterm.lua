80bf9df0a83cd40c6df978c36a271aa9ac7931eb
jorge <jorge@CL-IT02528.>
2025-02-15 09:20:45 -0300

    fterm toggle

--- lua/custom/plugins/fterm.lua
+++ lua/custom/plugins/fterm.lua
@@ -5,4 +5,9 @@ return {
     border = 'rounded',
     cmd = utils.is_wsl() and 'zsh' or 'pwsh',
   },
+  config = function(_, opts)
+    require('FTerm').setup(opts)
+    vim.keymap.set('n', '<c-t>', '<CMD>lua require("FTerm").toggle()<CR>')
+    vim.keymap.set('t', '<c-t>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+  end,
 }
