a323e10f133189afee8285b761ed26a57c0f7454
jorge <jorge@CL-IT02528.>
2025-02-16 10:38:36 -0300

    updated snacks

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -2,10 +2,9 @@
 return {
   'folke/snacks.nvim',
   opts = {
+    input = {},
     picker = {
-      -- your picker configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      ui_select = true,
       layout = {
         layout = {
           box = 'horizontal',
