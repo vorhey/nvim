24be86c709a3b9c318006c9e7d61766e0adae6e9
vorhey <jorgeherreraulloa@gmail.com>
2025-02-22 19:59:20 -0300

    git keybinding descriptions

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -20,31 +20,31 @@ return {
       },
       on_attach = function(buffer)
         local gs = require 'gitsigns'
-        vim.keymap.set('n', '<leader>gR', gs.reset_buffer, { desc = 'git: Restore' })
-        vim.keymap.set('n', '<leader>gr', gs.reset_hunk, { desc = 'git: Reset Hunk' })
-        vim.keymap.set('n', '<leader>gp', gs.preview_hunk, { desc = 'git: Preview Hunk' })
+        vim.keymap.set('n', '<leader>gR', gs.reset_buffer, { desc = 'git: restore' })
+        vim.keymap.set('n', '<leader>gr', gs.reset_hunk, { desc = 'git: reset hunk' })
+        vim.keymap.set('n', '<leader>gp', gs.preview_hunk, { desc = 'git: preview hunk' })
         vim.keymap.set('n', '<leader>gb', function()
           gs.blame_line { full = true }
-        end, { desc = 'git: Blame line' })
-        vim.keymap.set('n', '<leader>gB', gs.toggle_current_line_blame, { desc = 'git: Toggle Blame' })
+        end, { desc = 'git: blame line' })
+        vim.keymap.set('n', '<leader>gB', gs.toggle_current_line_blame, { desc = 'git: toggle blame' })
         vim.keymap.set('n', ']c', function()
           gs.nav_hunk('next', {
             target = 'all',
           })
-        end, { desc = 'git: Next Hunk' })
+        end, { desc = 'git: next hunk' })
         vim.keymap.set('n', '[c', function()
           gs.nav_hunk('prev', {
             target = 'all',
           })
-        end, { desc = 'git: Previous Hunk' })
-        vim.keymap.set('n', '<leader>gd', gs.diffthis, { desc = 'git: Toggle diff' })
+        end, { desc = 'git: previous hunk' })
+        vim.keymap.set('n', '<leader>gd', gs.diffthis, { desc = 'git: toggle diff' })
         vim.keymap.set('n', '<leader>gD', function()
           vim.ui.input({ prompt = 'Branch: ' }, function(branch)
             if branch then
               gs.diffthis(branch, { vertical = true })
             end
           end)
-        end, { desc = 'Git: Diff against branch' })
+        end, { desc = 'git: diff against branch' })
       end,
     }
   end,
