0e6c17b665a50640c598fe0f7500b9b56a222334
vorhey <jorge@CL-IT02528.>
2025-02-22 17:09:57 -0300

    change git keybinding to leader g

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -20,13 +20,13 @@ return {
       },
       on_attach = function(buffer)
         local gs = require 'gitsigns'
-        vim.keymap.set('n', '<leader>hR', gs.reset_buffer, { desc = 'git: Restore' })
-        vim.keymap.set('n', '<leader>hr', gs.reset_hunk, { desc = 'git: Reset Hunk' })
-        vim.keymap.set('n', '<leader>hp', gs.preview_hunk, { desc = 'git: Preview Hunk' })
-        vim.keymap.set('n', '<leader>hb', function()
+        vim.keymap.set('n', '<leader>gR', gs.reset_buffer, { desc = 'git: Restore' })
+        vim.keymap.set('n', '<leader>gr', gs.reset_hunk, { desc = 'git: Reset Hunk' })
+        vim.keymap.set('n', '<leader>gp', gs.preview_hunk, { desc = 'git: Preview Hunk' })
+        vim.keymap.set('n', '<leader>gb', function()
           gs.blame_line { full = true }
         end, { desc = 'git: Blame line' })
-        vim.keymap.set('n', '<leader>hB', gs.toggle_current_line_blame, { desc = 'git: Toggle Blame' })
+        vim.keymap.set('n', '<leader>gB', gs.toggle_current_line_blame, { desc = 'git: Toggle Blame' })
         vim.keymap.set('n', ']c', function()
           gs.nav_hunk('next', {
             target = 'all',
@@ -37,8 +37,8 @@ return {
             target = 'all',
           })
         end, { desc = 'git: Previous Hunk' })
-        vim.keymap.set('n', '<leader>hd', gs.diffthis, { desc = 'git: Toggle diff' })
-        vim.keymap.set('n', '<leader>hD', function()
+        vim.keymap.set('n', '<leader>gd', gs.diffthis, { desc = 'git: Toggle diff' })
+        vim.keymap.set('n', '<leader>gD', function()
           vim.ui.input({ prompt = 'Branch: ' }, function(branch)
             if branch then
               gs.diffthis(branch, { vertical = true })
