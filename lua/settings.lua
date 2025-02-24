75ee3dcaf78f849cb69db9c125bfc9e42c659d7b
vorhey <jorgeherreraulloa@gmail.com>
2025-02-24 00:15:33 -0300

    organized settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,151 +1,82 @@
 local utils = require 'utils'
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+
+-- Leader key configuration
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- Display and UI settings
+vim.opt.number = true -- Enable line numbers
+vim.opt.relativenumber = true -- Enable relative line numbers
+vim.opt.showmode = false -- Don't show mode in command line
+vim.opt.termguicolors = true -- Enable terminal GUI colors
+vim.opt.cursorline = true -- Highlight current line
+vim.opt.signcolumn = 'yes' -- Always show sign column
+vim.opt.showmatch = true -- Show matching brackets
+vim.opt.showcmd = false -- Don't show command in status line
+vim.opt.ruler = false -- Disable ruler
+vim.opt.cmdheight = 1 -- Command line height
 
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+-- Window and split behavior
+vim.opt.splitright = true -- Open new splits to the right
+vim.opt.splitbelow = true -- Open new splits below
+vim.opt.scrolloff = 10 -- Lines of context above/below cursor
+vim.opt.pumheight = 12 -- Maximum number of popup menu items
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+-- Search settings
+vim.opt.hlsearch = true -- Highlight search results
+vim.opt.ignorecase = true -- Case-insensitive searching
+vim.opt.smartcase = true -- Case-sensitive if search has capitals
+vim.opt.inccommand = 'nosplit' -- Preview substitutions
+vim.o.wildignorecase = true -- Ignore case in file/command completion
 
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.opt.hlsearch = true
-
-vim.opt.shiftwidth = 2
-
-vim.opt.tabstop = 2
-
-vim.opt.expandtab = true
-
-vim.opt.autoindent = true
-
-vim.opt.smartindent = true
-
-vim.opt.showcmd = false
-
--- remove ~ from empty lines
-vim.opt.fillchars = {
-  foldopen = '',
-  foldclose = '',
-  fold = ' ',
-  foldsep = ' ',
-  diff = '╱',
-  eob = ' ',
+-- Indentation and whitespace
+vim.opt.shiftwidth = 2 -- Size of indent
+vim.opt.tabstop = 2 -- Size of tab
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.autoindent = true -- Copy indent from current line
+vim.opt.smartindent = true -- Smart autoindenting
+vim.opt.breakindent = true -- Maintain indent when wrapping
+vim.opt.list = true -- Show whitespace characters
+vim.opt.listchars = {
+  tab = '» ',
+  trail = '·',
+  nbsp = '␣',
 }
 
--- ignore casing
-vim.o.wildignorecase = true
+-- Text wrapping
+vim.opt.wrap = true -- Enable line wrapping
+vim.opt.linebreak = true -- Wrap at word boundaries
+vim.opt.breakindent = true -- Preserve indentation in wrapped text
 
--- relative line numbers
-vim.wo.relativenumber = true
-
--- folding settings
--- Start with all folds open
-vim.o.foldlevelstart = 99
-
--- Enable folding
-vim.o.foldenable = true
-
--- Show the fold column; '0' means it's not displayed
-vim.o.foldcolumn = '0'
-
--- Keep folds open unless they are deep
-vim.o.foldlevel = 99
-
--- Set noruler
-vim.o.ruler = false
-
--- Terminal gui colors
-vim.opt.termguicolors = true
-
--- Wrap lines
-vim.opt.wrap = true
-vim.opt.linebreak = true
-vim.opt.breakindent = true
-
--- Matching brackets
-vim.opt.showmatch = true
-
--- Popup
-vim.opt.pumheight = 12 -- Maximum number of entries in a popup
-
--- Shortmess
-vim.opt.shortmess:append { W = true, I = true, c = true, C = true }
-
--- Cmdheight
-vim.o.cmdheight = 1
-
-vim.opt.ttyfast = true
-
--- Undercurl
-vim.cmd [[let &t_Cs = "\e[4:3m"]]
-vim.cmd [[let &t_Ce = "\e[4:0m"]]
-
-vim.opt.winbar = '%=' .. utils.get_root_dirname() .. "%{%v:lua.vim.fn.expand('%')%}%="
-
-vim.opt.inccommand = 'nosplit'
-
-vim.opt.foldmethod = 'expr'
-
+-- Folding configuration
+vim.o.foldenable = true -- Enable folding
+vim.o.foldlevelstart = 99 -- Start with all folds open
+vim.o.foldcolumn = '0' -- Hide fold column
+vim.o.foldlevel = 99 -- Keep folds open unless deep
+vim.opt.foldmethod = 'expr' -- Use treesitter for folding
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 
-vim.opt.backupcopy = 'yes'
+-- Performance and technical settings
+vim.opt.updatetime = 250 -- Faster completion
+vim.opt.timeoutlen = 300 -- Time to wait for mapped sequence
+vim.opt.ttyfast = true -- Faster terminal connection
+vim.opt.undofile = true -- Save undo history
+vim.opt.backupcopy = 'yes' -- Backup file copying method
+vim.opt.mouse = 'a' -- Enable mouse support
+
+-- Status line and window bar
+vim.opt.shortmess:append { W = true, I = true, c = true, C = true }
+vim.opt.winbar = '%=' .. utils.get_root_dirname() .. "%{%v:lua.vim.fn.expand('%')%}%="
+
+-- Remove tilde on empty lines and set fillchars
+vim.opt.fillchars = {
+  eob = ' ', -- Empty lines at the end of buffer
+  fold = ' ', -- Filling fold lines
+  foldsep = ' ', -- Separator between folds
+  diff = '╱', -- Deleted lines in diff
+}
+
+-- Terminal specific settings
+vim.cmd [[let &t_Cs = "\e[4:3m"]] -- Enable undercurl
+vim.cmd [[let &t_Ce = "\e[4:0m"]]
