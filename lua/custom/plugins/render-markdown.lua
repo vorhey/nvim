5df1cc7dabe62d2ebc9dd83a93eea5065f2cd0fc
vorhey <jorgeherreraulloa@gmail.com>
2025-03-04 01:19:40 -0300

    update render markdown

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -3,5 +3,10 @@ return {
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    code = {
+      highlight = 'Normal',
+      border = 'none',
+    },
+  },
 }
