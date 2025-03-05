571346642db611b8bebfe99070b8be7d71e36796
vorhey <jorgeherreraulloa@gmail.com>
2025-03-04 23:37:13 -0300

    java spotless sync formatting

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -63,6 +63,22 @@ return {
 
     conform.setup {
       notify_on_error = false,
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        local filetype = vim.bo[bufnr].filetype
+        -- Java files with spotless need synchronous formatting
+        if filetype == 'java' then
+          return {
+            async = false,
+            timeout_ms = 15000,
+            lsp_format = 'fallback',
+          }
+        end
+        -- Return nil for other filetypes to not use format_on_save
+        return nil
+      end,
       format_after_save = function(bufnr)
         if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
           return
