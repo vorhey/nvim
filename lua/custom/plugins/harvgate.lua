48f71335ce40f89af88e1e7f8a610ed1b218e330
vorhey <jorgeherreraulloa@gmail.com>
2025-03-09 10:24:15 -0300

    harvgate more width

--- lua/custom/plugins/harvgate.lua
+++ lua/custom/plugins/harvgate.lua
@@ -4,7 +4,9 @@ return {
     'nvim-lua/plenary.nvim',
   },
   config = function()
-    require('harvgate').setup {}
+    require('harvgate').setup {
+      width = 70,
+    }
     vim.keymap.set('n', '<leader>a', ':HarvgateChat<CR>', { desc = 'harvgate' })
   end,
 }
