4e0121cde84d1f6b1f106bcf22348c01e3a3eee7
vorhey <jorgeherreraulloa@gmail.com>
2025-03-15 14:13:09 -0300

    harvgate height

--- lua/custom/plugins/harvgate.lua
+++ lua/custom/plugins/harvgate.lua
@@ -6,6 +6,7 @@ return {
   config = function()
     require('harvgate').setup {
       width = 70,
+      height = 10,
     }
     vim.keymap.set('n', '<leader>a', ':HarvgateChat<CR>', { desc = 'harvgate' })
     vim.keymap.set('n', '<leader>A', ':HarvgateListChats<CR>', { desc = 'harvgate chat list' })
