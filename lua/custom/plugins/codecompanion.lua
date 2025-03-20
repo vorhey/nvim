48750a879c969ed53b642f5c80c562180f4fac80
vorhey <jorgeherreraulloa@gmail.com>
2025-03-20 00:20:45 -0300

    codecompanion add adapter

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -5,6 +5,15 @@ return {
     'nvim-treesitter/nvim-treesitter',
   },
   opts = {
+    adapters = {
+      copilot = {
+        schema = {
+          model = {
+            default = 'claude-3.5-sonnet',
+          },
+        },
+      },
+    },
     strategies = {
       chat = {
         adapter = 'copilot',
