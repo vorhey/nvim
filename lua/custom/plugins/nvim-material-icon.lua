37eaf941ebafa9ada0e3b039cc7a01ccb26982ec
vorhey <jorgeherreraulloa@gmail.com>
2025-03-20 22:17:37 -0300

    add verylazy lspconfig and icons

--- lua/custom/plugins/nvim-material-icon.lua
+++ lua/custom/plugins/nvim-material-icon.lua
@@ -1,5 +1,7 @@
 return {
   'DaikyXendo/nvim-material-icon',
+  lazy = true,
+  event = 'VeryLazy', -- Load after initial UI setup
   config = function()
     require('nvim-web-devicons').setup()
   end,
