7f50414af44978504a4e2fb09d0567627368c97b
vorhey <jorgeherreraulloa@gmail.com>
2025-03-25 23:24:52 -0300

    update toggle dbui close opened buffers

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -42,6 +42,11 @@ return {
               return vim.g.db_ui_open
             end,
             set = function()
+              for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_name(buf) == '' then
+                  vim.api.nvim_buf_delete(buf, { force = true })
+                end
+              end
               vim.cmd 'DBUIToggle'
             end,
           })
