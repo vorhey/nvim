83872b11c6a38bcab638497d344aa54ca8f19fb5
vorhey <jorgeherreraulloa@gmail.com>
2025-03-26 10:00:55 -0300

    add avante removed codecompanion

---
+++ lua/custom/plugins/avante.lua
@@ -0,0 +1,37 @@
+return {
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
+  version = false, -- Never set this value to "*"! Never!
+  opts = {
+    -- gemini-2.5-pro-exp-03-25
+    -- add any opts here
+    -- for example
+    provider = 'gemini',
+    gemini = {
+      model = 'gemini-2.5-pro-exp-03-25', -- your desired model (or use gpt-4o, etc.)
+      timeout = 60000, -- Timeout in milliseconds, increase this for reasoning models
+      temperature = 0,
+      -- max_completion_tokens = 16384, -- Increase this to include reasoning tokens (for reasoning models)
+      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = 'make',
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'stevearc/dressing.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    --- The below dependencies are optional,
+    'zbirenbaum/copilot.lua', -- for providers='copilot'
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
+      },
+      ft = { 'markdown', 'Avante' },
+    },
+  },
+}
