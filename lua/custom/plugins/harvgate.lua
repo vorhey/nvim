a7b02149c354be7aae057cd1b81a7738352e02d5
vorhey <jorgeherreraulloa@gmail.com>
2025-03-27 21:02:10 -0300

    replaced snacks notifier with mini notify

--- lua/custom/plugins/harvgate.lua
+++ lua/custom/plugins/harvgate.lua
@@ -8,8 +8,8 @@ return {
       width = 70,
       height = 10,
     }
-    vim.keymap.set('n', '<leader>hh', ':HarvgateChat<CR>', { desc = 'harvgate toggle chat' })
-    vim.keymap.set('n', '<leader>hc', ':HarvgateListChats<CR>', { desc = 'harvgate open chats' })
-    vim.keymap.set('n', '<leader>ha', ':HarvgateAddBuffer<CR>', { desc = 'harvgate add buffer' })
+    vim.keymap.set('n', '<leader>hh', ':HarvgateChat<CR>', { desc = 'harvgate toggle chat', silent = true })
+    vim.keymap.set('n', '<leader>hc', ':HarvgateListChats<CR>', { desc = 'harvgate open chats', silent = true })
+    vim.keymap.set('n', '<leader>ha', ':HarvgateAddBuffer<CR>', { desc = 'harvgate add buffer', silent = true })
   end,
 }
