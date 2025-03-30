e2b4dffa3e45dee224b2f17fbd6a28d1f77afaff
vorhey <jorgeherreraulloa@gmail.com>
2025-03-29 22:50:01 -0300

    add lazydev provider blink cmp

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -37,6 +37,12 @@ return {
         sql = { 'snippets', 'dadbod', 'buffer' },
       },
       providers = {
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
+        },
         dadbod = {
           name = 'Dadbod',
           module = 'vim_dadbod_completion.blink',
