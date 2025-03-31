7e0f847adbbb6685412fd94a07890f7b8ae6ace5
vorhey <jorgeherreraulloa@gmail.com>
2025-03-31 08:11:59 -0300

    notify add keybinding show history

--- lua/custom/plugins/mini.notify.lua
+++ lua/custom/plugins/mini.notify.lua
@@ -35,5 +35,8 @@ return {
     }
 
     vim.notify = require('mini.notify').make_notify()
+    vim.keymap.set('n', '<leader>fn', function()
+      require('mini.notify').show_history()
+    end, { desc = 'Show notification history' })
   end,
 }
