f6dc87c622b0b7687ac4e4675af5dcc2523c2270
vorhey <jorgeherreraulloa@gmail.com>
2025-04-05 06:52:35 -0300

    add bufferlist keybindings

--- lua/custom/plugins/bufferlist.lua
+++ lua/custom/plugins/bufferlist.lua
@@ -3,5 +3,27 @@ return {
   lazy = true,
   keys = { { '<leader><leader>', ':BufferList<CR>', desc = 'Open bufferlist' } },
   cmd = 'BufferList',
-  opts = {},
+  opts = {
+    win_keymaps = {
+      {
+        '<C-x>', -- Ctrl+x to close buffer under cursor
+        function(opts)
+          local curpos = vim.fn.line '.'
+          local buf_id = opts.buffers[curpos]
+          vim.cmd('bwipeout | silent! bdelete ' .. buf_id)
+          -- Reopen bufferlist to refresh
+          opts.open_bufferlist()
+        end,
+        { desc = 'BufferList: close buffer under cursor' },
+      },
+      {
+        '<cr>', -- Enter key to open buffer under cursor
+        function(opts)
+          local curpos = vim.fn.line '.'
+          vim.cmd('bwipeout | buffer ' .. opts.buffers[curpos])
+        end,
+        { desc = 'BufferList: open buffer under cursor' },
+      },
+    },
+  },
 }
