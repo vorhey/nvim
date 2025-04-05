69f3eedba10a46bdff8a108c182b1f9e285a1a73
vorhey <jorgeherreraulloa@gmail.com>
2025-04-05 01:06:40 -0300

    add info icon diagnostic

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -45,6 +45,7 @@ return {
           [vim.diagnostic.severity.ERROR] = '󰅗',
           [vim.diagnostic.severity.WARN] = '󰩳',
           [vim.diagnostic.severity.HINT] = '',
+          [vim.diagnostic.severity.INFO] = '',
         },
       },
     }
