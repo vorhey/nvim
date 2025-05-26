e176c0abaea5731f8838a0a6a5cd17096e270b44
vorhey <jorgeherreraulloa@gmail.com>
2025-05-26 02:43:50 -0400

    add oil

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+  'stevearc/oil.nvim',
+  config = function()
+    ---@type oil.SetupOpts
+    require('oil').setup {}
+    vim.keymap.set('n', '<leader>e', function()
+      require('oil').open_float()
+    end, { desc = 'Open Oil in float' })
+  end,
+  -- Optional dependencies
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  lazy = false,
+}
