40ed1794f06f445ee4d1a31def8c97411760a0d4
vorhey <jorge.herrera01@globant.com>
2025-06-08 15:37:13 -0400

    remove autopairs default rules

--- lua/custom/plugins/nvim-autopairs.lua
+++ lua/custom/plugins/nvim-autopairs.lua
@@ -5,6 +5,9 @@ return {
     local npairs = require 'nvim-autopairs'
     local Rule = require 'nvim-autopairs.rule'
     npairs.setup {}
+    for _, i in ipairs(npairs.config.rules) do
+      i.key_map = nil
+    end
     npairs.clear_rules()
     -- Add only the double quote rule with flyout behavior
     npairs.add_rule(Rule('"', '"'):with_move(function(opts)
