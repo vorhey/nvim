e7ddc28e03d8bee8ebd194b3e3ba592ea763bab2
vorhey <jorge.herrera01@globant.com>
2025-06-09 00:02:50 -0400

    updated keys descriptions

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -3,9 +3,9 @@ vim.keymap.set('n', '<leader>Q', ':qa!<CR>', { desc = 'exit (all)' })
 vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'exit' })
 
 if not vim.g.is_wsl then
-  vim.keymap.set({ 'n', 'v' }, '<C-c>', '"+y', { silent = true, desc = 'Copy to system clipboard' })
+  vim.keymap.set({ 'n', 'v' }, '<C-c>', '"+y', { silent = true, desc = 'copy to system clipboard' })
 end
 
-vim.keymap.set('i', '<C-r>"', '<C-r><C-p>"', { desc = 'Paste default register with indent' })
+vim.keymap.set('i', '<C-r>"', '<C-r><C-p>"', { desc = 'paste default register with indent' })
 
 vim.keymap.set('n', '<C-m>', 'f{a<cr><esc>O', { desc = 'grow braces' })
