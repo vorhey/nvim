4bc1338bf03a9328e7a06cbcae4ff277381182ef
vorhey <jorgeherreraulloa@gmail.com>
2025-06-11 01:20:03 -0400

    add copy file to system clipboard

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -7,6 +7,21 @@ return {
         padding = 0,
         max_height = 0.97,
       },
+      keymaps = {
+        ['gyy'] = {
+          desc = 'copy filepath to system clipboard',
+          callback = function()
+            local oil = require 'oil'
+            local entry = oil.get_cursor_entry()
+            local dir = oil.get_current_dir()
+            if not entry or not dir then
+              return
+            end
+            local relpath = vim.fn.fnamemodify(dir, ':.')
+            vim.fn.setreg('+', relpath .. entry.name)
+          end,
+        },
+      },
     }
     vim.keymap.set('n', '<leader>e', function()
       require('oil').open_float()
