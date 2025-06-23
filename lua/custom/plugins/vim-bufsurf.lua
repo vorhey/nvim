e342269f9bdd8f0a16e12630c7671326d5755a54
vorhey <jorge.herrera01@globant.com>
2025-06-23 10:01:21 -0400

    remove bufsurf

--- lua/custom/plugins/vim-bufsurf.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'ton/vim-bufsurf',
-  config = function()
-    vim.keymap.set('n', '[b', '<Plug>(buf-surf-back)', { desc = 'Previous buffer' })
-    vim.keymap.set('n', ']b', '<Plug>(buf-surf-forward)', { desc = 'Next buffer' })
-  end,
-}
