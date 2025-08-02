6ff70514a17db9007631d2261826316123dc1311
vorhey <jorgeherreraulloa@gmail.com>
2025-08-01 23:42:04 -0400

    avoid closing pair next to character

--- lua/custom/plugins/ultimate-autopair.lua
+++ lua/custom/plugins/ultimate-autopair.lua
@@ -2,6 +2,23 @@ return {
   'altermo/ultimate-autopair.nvim',
   event = { 'InsertEnter', 'CmdlineEnter' },
   opts = {
+    extensions = {
+      cond = {
+        filter = true,
+        cond = function()
+          local line = vim.api.nvim_get_current_line()
+          local col = vim.api.nvim_win_get_cursor(0)[2]
+          local char_before = col > 0 and line:sub(col, col) or ''
+          local char_after = col < #line and line:sub(col + 1, col + 1) or ''
+
+          if char_before:match '%w' or char_after:match '%w' then
+            return false
+          end
+
+          return true
+        end,
+      },
+    },
     internal_pairs = {
       { '[', ']', fly = true, dosuround = true, suround = true, newline = true, space = true },
       { '(', ')', fly = true, dosuround = true, suround = true, newline = true, space = true },
