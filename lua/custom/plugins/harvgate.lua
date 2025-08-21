7e3e2bf8d8effd0d80fbc95aec808808301b6046
vorhey <jorgeherreraulloa@gmail.com>
2025-08-21 17:48:04 -0400

    harvgate claude opus

--- lua/custom/plugins/harvgate.lua
+++ lua/custom/plugins/harvgate.lua
@@ -9,6 +9,7 @@ return {
     require('harvgate').setup {
       width = 70,
       height = 10,
+      model = 'claude-opus-4-1-20250805',
     }
     vim.keymap.set('n', '<leader>hh', ':HarvgateChat<CR>', { desc = 'harvgate toggle chat', silent = true })
     vim.keymap.set('n', '<leader>hc', ':HarvgateListChats<CR>', { desc = 'harvgate open chats', silent = true })
