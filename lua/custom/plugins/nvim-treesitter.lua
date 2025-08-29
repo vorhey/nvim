36ce1f98736fe1a6ff9d7b3b932c237dacced8db
vorhey <jorgeherreraulloa@gmail.com>
2025-08-29 14:53:10 -0400

    add treesitter descriptions

--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -54,17 +54,13 @@ return {
           enable = true,
           lookahead = true,
           keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            -- You can optionally set descriptions to the mappings (used in the desc parameter of
-            -- nvim_buf_set_keymap) which plugins like which-key display
-            ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
-            -- You can also use captures from other query groups like `locals.scm`
-            ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
-            ['ia'] = '@parameter.inner', -- Select inner part of an argument
-            ['aa'] = '@parameter.outer', -- Select the argument including surrounding commas
+            ['af'] = { query = '@function.outer', desc = 'select around function' },
+            ['if'] = { query = '@function.inner', desc = 'select inner function' },
+            ['ac'] = { query = '@class.outer', desc = 'select around class' },
+            ['ic'] = { query = '@class.inner', desc = 'select inner part of a class region' },
+            ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'select language scope' },
+            ['ia'] = { query = '@parameter.inner', desc = 'select inner parameter' },
+            ['aa'] = { query = '@parameter.outer', desc = 'select around parameter (with commas)' },
           },
         },
       },
