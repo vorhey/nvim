d642c240c37249f2dc63494b95e3e6c5cd0c5e33
vorhey <jorgeherreraulloa@gmail.com>
2025-09-07 15:19:01 -0300

    add code-bridge shortcuts

--- lua/custom/plugins/code-bridge.lua
+++ lua/custom/plugins/code-bridge.lua
@@ -5,5 +5,8 @@ return {
       provider = 'codex', -- 'claude' | 'codex' | 'gemini' (default 'claude')
       interactive = { use_telescope = true },
     }
+    vim.keymap.set('n', '<leader>cf', '<cmd>CodeBridgeTmux<cr>', { desc = 'code-bridge single file' })
+    vim.keymap.set('n', '<leader>ca', '<cmd>CodeBridgeTmuxAll<cr>', { desc = 'code-bridge all files' })
+    vim.keymap.set('n', '<leader>ci', '<cmd>CodeBridgeTmuxInteractive<cr>', { desc = 'code-bridge interactive' })
   end,
 }
