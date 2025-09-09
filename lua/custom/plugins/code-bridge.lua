6de140a22a91e9b4528c2cad89dfa0d58a1f500b
vorhey <jorgeherreraulloa@gmail.com>
2025-09-09 02:00:13 -0300

    updated code-bridge description keys

--- lua/custom/plugins/code-bridge.lua
+++ lua/custom/plugins/code-bridge.lua
@@ -4,11 +4,11 @@ return {
     require('code-bridge').setup {
       provider = 'codex', -- 'claude' | 'codex' | 'gemini' (default 'claude')
     }
-    vim.keymap.set('n', '<leader>cc', ':CodeBridgeAddContextAndSwitch<CR>', { desc = 'Send file or selection' })
-    vim.keymap.set('v', '<leader>cc', ':CodeBridgeAddContextAndSwitch<CR>', { desc = 'Send selection' })
-    vim.keymap.set('n', '<leader>cf', ':CodeBridgeAddContext<CR>', { desc = 'Add file/selection (no switch)' })
-    vim.keymap.set('v', '<leader>cf', ':CodeBridgeAddContext<CR>', { desc = 'Add selection (no switch)' })
-    vim.keymap.set('n', '<leader>cd', ':CodeBridgeDiff<CR>', { desc = 'Send git diff' })
-    vim.keymap.set('n', '<leader>cs', ':CodeBridgeDiffStaged<CR>', { desc = 'Send staged diff' })
+    vim.keymap.set('n', '<leader>cc', ':CodeBridgeAddContextAndSwitch<CR>', { desc = 'send file or selection' })
+    vim.keymap.set('v', '<leader>cc', ':CodeBridgeAddContextAndSwitch<CR>', { desc = 'send selection' })
+    vim.keymap.set('n', '<leader>cf', ':CodeBridgeAddContext<CR>', { desc = 'add file/selection (no switch)' })
+    vim.keymap.set('v', '<leader>cf', ':CodeBridgeAddContext<CR>', { desc = 'add selection (no switch)' })
+    vim.keymap.set('n', '<leader>cd', ':CodeBridgeDiff<CR>', { desc = 'send git diff' })
+    vim.keymap.set('n', '<leader>cs', ':CodeBridgeDiffStaged<CR>', { desc = 'send staged diff' })
   end,
 }
