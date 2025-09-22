2e7e15f56c7af70f6d54123abafb5354e7fe380b
vorhey <jorgeherreraulloa@gmail.com>
2025-09-22 02:24:40 -0300

    add undotree

---
+++ lua/custom/plugins/undotree.lua
@@ -0,0 +1,13 @@
+return {
+  'jiaoshijie/undotree',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  ---@module 'undotree.collector'
+  ---@type UndoTreeCollector.Opts
+  opts = {
+    float_diff = true,
+    window = {
+      winblend = 0,
+    },
+  },
+  keys = { { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" } },
+}
