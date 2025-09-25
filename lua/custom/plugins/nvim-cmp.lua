0086cffa507813481327e4972c099480d51a7628
vorhey <jorgeherreraulloa@gmail.com>
2025-09-24 21:29:01 -0300

    removed luasnip

--- lua/custom/plugins/nvim-cmp.lua
+++ lua/custom/plugins/nvim-cmp.lua
@@ -4,16 +4,6 @@ return {
   event = { 'InsertEnter', 'CmdlineEnter' },
   enabled = true,
   dependencies = {
-    -- luasnip
-    {
-      'L3MON4D3/LuaSnip',
-      version = 'v2.*',
-      dependencies = {
-        'saadparwaiz1/cmp_luasnip',
-        'rafamadriz/friendly-snippets',
-      },
-      build = 'make install_jsregexp',
-    },
     { 'hrsh7th/cmp-nvim-lsp', event = 'InsertEnter' },
     { 'hrsh7th/cmp-buffer', event = 'InsertEnter' },
     { 'hrsh7th/cmp-path', event = 'InsertEnter' },
@@ -26,15 +16,6 @@ return {
     vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
     local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-
-    -- Load snippets
-    require('luasnip.loaders.from_vscode').lazy_load()
-
-    luasnip.filetype_extend('javascript', { 'jsdoc' })
-    luasnip.filetype_extend('typescript', { 'jsdoc' })
-
-    luasnip.config.setup { enable_autosnippets = true }
 
     -- Variable to track documentation window state
     local docs_enabled = false
@@ -63,30 +44,24 @@ return {
       ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.confirm { select = true }
-        elseif luasnip.expand_or_locally_jumpable() then
-          luasnip.expand_or_jump()
         else
           fallback()
         end
       end, { 'i', 's' }),
 
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
-        if luasnip.locally_jumpable(-1) then
-          luasnip.jump(-1)
+      ['<C-h>'] = cmp.mapping(function(fallback)
+        if vim.snippet.active({ direction = -1 }) then
+          vim.snippet.jump(-1)
         else
           fallback()
         end
       end, { 'i', 's' }),
 
-      ['<C-h>'] = cmp.mapping(function()
-        if luasnip.locally_jumpable(-1) then
-          luasnip.jump(-1)
-        end
-      end, { 'i', 's' }),
-
-      ['<C-l>'] = cmp.mapping(function()
-        if luasnip.locally_jumpable(1) then
-          luasnip.jump(1)
+      ['<C-l>'] = cmp.mapping(function(fallback)
+        if vim.snippet.active({ direction = 1 }) then
+          vim.snippet.jump(1)
+        else
+          fallback()
         end
       end, { 'i', 's' }),
     }
@@ -94,7 +69,6 @@ return {
     local sources = cmp.config.sources {
       { name = 'nvim_lsp' },
       { name = 'lazydev' },
-      { name = 'luasnip' },
       { name = 'path' },
       { name = 'nvim_lsp_signature_help' },
       { name = 'html-css' },
@@ -115,18 +89,13 @@ return {
         }(entry, item)
         local strings = vim.split(kind.kind, '%s', { trimempty = true })
         kind.kind = ' ' .. (strings[1] or '') .. ' '
-        kind.menu = '    (' .. (strings[2] or '') .. ')'
+        kind.menu = '    (' .. (strings[2] or '') .. ') [' .. entry.source.name .. ']'
         return kind
       end,
     }
 
     -- Main setup
     cmp.setup {
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
       mapping = cmp.mapping.preset.insert(keybinds),
       sources = sources,
       sorting = {
