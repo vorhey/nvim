77f817d4c87a12355bf0a144536bdd80e1f2bddc
vorhey <jorgeherreraulloa@gmail.com>
2025-09-27 04:01:08 -0300

    number colors gitsigns disable

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -1,9 +1,9 @@
 return {
   'lewis6991/gitsigns.nvim',
-  event = "VeryLazy",
+  event = 'VeryLazy',
   config = function()
     require('gitsigns').setup {
-      numhl = true,
+      -- numhl = true,
       signs = {
         add = { text = '▎' },
         change = { text = '▎' },
