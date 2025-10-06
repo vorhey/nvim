d47dbe63ea7773e388aa7b2bdec12690f50020c9
vorhey <jorgeherreraulloa@gmail.com>
2025-10-06 01:03:53 -0300

    compact codecompanion config

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -8,28 +8,16 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
-    "ravitemer/codecompanion-history.nvim",
-
-    {
-      'MeanderingProgrammer/render-markdown.nvim',
-      ft = { 'markdown', 'codecompanion' },
-    },
+    'ravitemer/codecompanion-history.nvim',
+    { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'markdown', 'codecompanion' } },
   },
   config = function()
     require('codecompanion').setup {
-      extensions = {
-        history = {
-          enabled = true
-        }
-      }
+      extensions = { history = { enabled = true } }
     }
-    vim.keymap.set({ 'n', 'v' }, '<C-b>', '<cmd>CodeCompanionActions<cr>',
-      { noremap = true, silent = true, desc = 'code companion: actions' })
-    vim.keymap.set({ 'n', 'v' }, '<leader>aa', '<cmd>CodeCompanionChat Toggle<cr>',
-      { noremap = true, silent = true, desc = 'code companion: toggle' })
-    vim.keymap.set('v', 'ga', '<cmd>CodeCompanionChat Add<cr>',
-      { noremap = true, silent = true, desc = 'code companion: chat add' })
-    -- Expand 'cc' into 'CodeCompanion' in the command line
+    vim.keymap.set({ 'n', 'v' }, '<C-b>', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true, desc = 'code companion: actions' })
+    vim.keymap.set({ 'n', 'v' }, '<leader>aa', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true, desc = 'code companion: toggle' })
+    vim.keymap.set('v', 'ga', '<cmd>CodeCompanionChat Add<cr>', { noremap = true, silent = true, desc = 'code companion: chat add' })
     vim.cmd [[cab cc CodeCompanion]]
   end,
 }
