de988d419b1c566c34494986fa6a01f0352b6f57
vorhey <jorgeherreraulloa@gmail.com>
2025-10-06 00:41:32 -0300

    readd codecompanion

---
+++ lua/custom/plugins/codecompanion.lua
@@ -0,0 +1,35 @@
+return {
+  'olimorris/codecompanion.nvim',
+  keys = {
+    { '<C-b>',      mode = { 'n', 'v' } },
+    { '<leader>aa', mode = { 'n', 'v' } },
+    { 'ga',         mode = 'v' },
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    "ravitemer/codecompanion-history.nvim",
+
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      ft = { 'markdown', 'codecompanion' },
+    },
+  },
+  config = function()
+    require('codecompanion').setup {
+      extensions = {
+        history = {
+          enabled = true
+        }
+      }
+    }
+    vim.keymap.set({ 'n', 'v' }, '<C-b>', '<cmd>CodeCompanionActions<cr>',
+      { noremap = true, silent = true, desc = 'code companion: actions' })
+    vim.keymap.set({ 'n', 'v' }, '<leader>aa', '<cmd>CodeCompanionChat Toggle<cr>',
+      { noremap = true, silent = true, desc = 'code companion: toggle' })
+    vim.keymap.set('v', 'ga', '<cmd>CodeCompanionChat Add<cr>',
+      { noremap = true, silent = true, desc = 'code companion: chat add' })
+    -- Expand 'cc' into 'CodeCompanion' in the command line
+    vim.cmd [[cab cc CodeCompanion]]
+  end,
+}
