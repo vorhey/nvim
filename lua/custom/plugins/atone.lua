739e013d31433d16d4a9489593152f2b92948882
vorhey <jorgeherreraulloa@gmail.com>
2025-10-31 09:28:05 -0300

    no lazy atone

--- lua/custom/plugins/atone.lua
+++ lua/custom/plugins/atone.lua
@@ -1,8 +1,7 @@
 return {
   'XXiaoA/atone.nvim',
-  cmd = 'Atone',
   config = function()
     require('atone').setup {}
-    vim.keymap.set('n', '<leader>u', '<cmd>Atone<cr>', { desc = 'Atone' })
+    vim.keymap.set('n', '<leader>u', '<cmd>Atone toggle<cr>', { desc = 'Atone' })
   end,
 }
