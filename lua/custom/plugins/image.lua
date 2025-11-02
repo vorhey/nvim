cc1e4b4cb9baa1c92bf9ce90893aade5ed589d64
vorhey <jorgeherreraulloa@gmail.com>
2025-11-02 16:19:53 -0300

    add image sixel support under wsl

---
+++ lua/custom/plugins/image.lua
@@ -0,0 +1,17 @@
+return {
+  '3rd/image.nvim',
+  enabled = vim.g.is_wsl,
+  build = false,
+  config = function()
+    require('image').setup {
+      backend = 'sixel',
+      processor = 'magick_cli',
+      integrations = {
+        markdown = {
+          only_render_image_at_cursor = true,
+          only_render_image_at_cursor_mode = 'popup',
+        },
+      },
+    }
+  end,
+}
