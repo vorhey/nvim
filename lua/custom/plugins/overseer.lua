3870cd415241b99987af60a3bf6afef0a56a178f
vorhey <jorgeherreraulloa@gmail.com>
2025-11-11 01:42:19 -0300

    keys overseer

--- lua/custom/plugins/overseer.lua
+++ lua/custom/plugins/overseer.lua
@@ -20,20 +20,5 @@ return {
     vim.keymap.set('n', '<leader>rt', function()
       overseer.toggle()
     end, { desc = 'Overseer: Toggle task list' })
-    vim.keymap.set('n', '<leader>rl', function()
-      overseer.load_template()
-    end, { desc = 'Overseer: Load template' })
-    vim.keymap.set('n', '<leader>rq', function()
-      overseer.quick_action()
-    end, { desc = 'Overseer: Quick action' })
-    vim.keymap.set('n', '<leader>rs', function()
-      overseer.run_action 'restart'
-    end, { desc = 'Overseer: Restart last task' })
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = { 'OverseerList', 'OverseerOutput' },
-      callback = function()
-        vim.opt_local.statusline = ' '
-      end,
-    })
   end,
 }
