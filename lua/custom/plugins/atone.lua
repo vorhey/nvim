de587ddb25e2a8f9e023d843236b5c40bb0776a5
vorhey <jorgeherreraulloa@gmail.com>
2025-12-15 01:30:04 -0300

    removed not used plugins

--- lua/custom/plugins/atone.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'XXiaoA/atone.nvim',
-  config = function()
-    require('atone').setup {}
-    vim.keymap.set('n', '<leader>u', '<cmd>Atone toggle<cr>', { desc = 'Atone' })
-  end,
-}
--- lua/custom/plugins/mermaid.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'selimacerbas/mermaid-playground.nvim',
-  lazy = true,
-  event = 'VeryLazy',
-  dependencies = { 'barrett-ruth/live-server.nvim', opts = {} },
-  config = function()
-    require('mermaid_playground').setup {}
-  end,
-}
