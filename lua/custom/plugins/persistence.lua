73399ef61ccb44d1250328867d6cc7038a4759b1
vorhey <jorgeherreraulloa@gmail.com>
2026-01-23 12:05:42 -0300

    persistance close buffers

--- lua/custom/plugins/persistence.lua
+++ lua/custom/plugins/persistence.lua
@@ -3,9 +3,15 @@ return {
   event = 'BufReadPre',
   opts = {},
   keys = {
-    { '<leader>ss', function() require('persistence').select() end, desc = 'Select session' },
+    { '<leader>ss', function()
+      vim.cmd('bufdo bwipeout')
+      require('persistence').select()
+    end, desc = 'Select session' },
     { '<leader>sw', function() require('persistence').save() end, desc = 'Write session' },
     { '<leader>sd', function() require('persistence').stop() end, desc = 'Stop session tracking' },
-    { '<leader>sc', function() require('persistence').load { last = true } end, desc = 'Load last session' },
+    { '<leader>sc', function()
+      vim.cmd('bufdo bwipeout')
+      require('persistence').load { last = true }
+    end, desc = 'Load last session' },
   },
 }
