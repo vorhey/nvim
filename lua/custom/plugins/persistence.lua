fe6a0623003148138d20761ce3f365f2fc7d12c4
vorhey <jorgeherreraulloa@gmail.com>
2026-01-23 20:05:23 -0300

    wipeout force on switching sessions persistance

--- lua/custom/plugins/persistence.lua
+++ lua/custom/plugins/persistence.lua
@@ -4,13 +4,13 @@ return {
   opts = {},
   keys = {
     { '<leader>ss', function()
-      vim.cmd('bufdo bwipeout')
+      vim.cmd('bufdo bwipeout!')
       require('persistence').select()
     end, desc = 'Select session' },
     { '<leader>sw', function() require('persistence').save() end, desc = 'Write session' },
     { '<leader>sd', function() require('persistence').stop() end, desc = 'Stop session tracking' },
     { '<leader>sc', function()
-      vim.cmd('bufdo bwipeout')
+      vim.cmd('bufdo bwipeout!')
       require('persistence').load { last = true }
     end, desc = 'Load last session' },
   },
