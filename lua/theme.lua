089636429d516b800cf0eede93624a69a715cd67
vorhey <jorgeherreraulloa@gmail.com>
2026-01-28 20:25:39 -0300

    updated neovide theme to cyberdream

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,22 +1,20 @@
 return {
   {
-    'sainnhe/everforest',
+    'scottmckendry/cyberdream.nvim',
     lazy = false,
     priority = 1000,
     cond = function()
       return vim.g.neovide
     end,
     config = function()
-      vim.g.everforest_background = 'hard'
-      vim.g.everforest_ui_contrast = 'high'
-      vim.g.everforest_float_style = 'blend'
-      vim.g.everforest_colors_override = { bg1 = { 'none', 'none' } }
-      vim.cmd.colorscheme 'everforest'
+      vim.cmd.colorscheme 'cyberdream'
       vim.api.nvim_set_hl(0, 'BlinkCmpMenuBorder', { bg = 'none' })
       vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
       vim.api.nvim_set_hl(0, 'Float', { bg = 'none' })
       vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'Red', { fg = '#ffdfb8' })
+      vim.api.nvim_set_hl(0, 'Keyword', { fg = '#8FA6FF' })
+      vim.api.nvim_set_hl(0, 'Number', { fg = '#8FFFF8' })
+      vim.api.nvim_set_hl(0, 'Operator', { fg = '#FFBDE1' })
       vim.api.nvim_set_hl(0, 'SnacksPicker', { bg = 'none' })
       vim.api.nvim_set_hl(0, 'SnacksPickerBorder', { bg = 'none' })
       vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'none' })
