3099ca9d7ea618ecd1315f56f0b17817a58ef7aa
vorhey <jorgeherreraulloa@gmail.com>
2026-03-06 15:40:12 -0300

    disable when touch enabled for autoclose

--- lua/custom/plugins/autoclose.lua
+++ lua/custom/plugins/autoclose.lua
@@ -19,7 +19,7 @@ return {
       },
       options = {
         disabled_filetypes = { 'text' },
-        disable_when_touch = false,
+        disable_when_touch = true,
         touch_regex = '[%w(%[{]',
         pair_spaces = false,
         auto_indent = true,
