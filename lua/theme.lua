72cb57778315061f60d412517f04c0ca605cb3e6
vorhey <jorgeherreraulloa@gmail.com>
2026-04-07 15:34:01 -0400

    removed neovide theme specific setting

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,27 +1,5 @@
 return {
   {
-    'scottmckendry/cyberdream.nvim',
-    lazy = false,
-    priority = 1000,
-    cond = function()
-      return vim.g.neovide
-    end,
-    config = function()
-      vim.cmd.colorscheme 'cyberdream'
-      vim.api.nvim_set_hl(0, 'BlinkCmpMenuBorder', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'Float', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'Keyword', { fg = '#8FA6FF' })
-      vim.api.nvim_set_hl(0, 'Number', { fg = '#8FFFF8' })
-      vim.api.nvim_set_hl(0, 'Operator', { fg = '#FFBDE1' })
-      vim.api.nvim_set_hl(0, 'Type', { fg = '#AE90f0' })
-      vim.api.nvim_set_hl(0, 'SnacksPicker', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'SnacksPickerBorder', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'none' })
-    end,
-  },
-  {
     'vorhey/oldworld.nvim',
     lazy = false,
     priority = 1000,
