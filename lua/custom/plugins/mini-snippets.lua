96ea0bc4d07db89d4eb5cd07e24bcbce18ba841f
vorhey <jorgeherreraulloa@gmail.com>
2026-04-20 15:58:28 -0400

    updated mini snippet keybindings

--- lua/custom/plugins/mini-snippets.lua
+++ lua/custom/plugins/mini-snippets.lua
@@ -9,27 +9,8 @@ return {
         snippets.gen_loader.from_lang(),
       },
     }
-
-    vim.keymap.set('i', '<C-s>', function()
+    vim.keymap.set('i', '<M-s>', function()
       snippets.expand { match = false }
     end, { desc = 'snippets: pick and expand' })
-
-    vim.keymap.set('i', '<Tab>', function()
-      local session = snippets.get_session()
-      if session then
-        snippets.session.jump 'next'
-      else
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
-      end
-    end, { desc = 'snippets: jump next tabstop' })
-
-    vim.keymap.set('i', '<S-Tab>', function()
-      local session = snippets.get_session()
-      if session then
-        snippets.session.jump 'prev'
-      else
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<S-Tab>', true, false, true), 'n', false)
-      end
-    end, { desc = 'snippets: jump prev tabstop' })
   end,
 }
