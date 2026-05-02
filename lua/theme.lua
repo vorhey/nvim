aa84e267b2111680204b3e7705b7d5493f0a6fd1
vorhey <jorgeherreraulloa@gmail.com>
2026-05-01 22:21:14 -0400

    updated theme for neovide

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,24 +1,33 @@
 return {
   {
-    'scottmckendry/cyberdream.nvim',
-    lazy = false,
-    priority = 1000,
     cond = function()
       return vim.g.neovide
     end,
+    'daedlock/matugen.nvim',
+    lazy = false,
+    priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'cyberdream'
-      vim.api.nvim_set_hl(0, 'BlinkCmpMenuBorder', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'Float', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'Keyword', { fg = '#8FA6FF' })
-      vim.api.nvim_set_hl(0, 'Number', { fg = '#8FFFF8' })
-      vim.api.nvim_set_hl(0, 'Operator', { fg = '#FFBDE1' })
-      vim.api.nvim_set_hl(0, 'Type', { fg = '#AE90f0' })
-      vim.api.nvim_set_hl(0, 'SnacksPicker', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'SnacksPickerBorder', { bg = 'none' })
-      vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'none' })
+      require('matugen').setup {
+        colors_path = '~/.config/matugen/colors-empty.json',
+        contrast = 0.08,
+        -- min_contrast = 0.1,
+        brightness = -0.02,
+        -- sidebar_brightness = -0.04,
+      }
+      vim.cmd.colorscheme 'matugen'
+
+      local function transparent_statusline()
+        vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'NONE' })
+        vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'NONE' })
+        vim.api.nvim_set_hl(0, 'MiniStatuslineFilename', { bg = 'NONE' })
+        vim.api.nvim_set_hl(0, 'MiniStatuslineDevinfo', { bg = 'NONE' })
+        vim.api.nvim_set_hl(0, 'MiniStatuslineInactive', { bg = 'NONE' })
+      end
+      transparent_statusline()
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MatugenReloaded',
+        callback = transparent_statusline,
+      })
     end,
   },
   {
