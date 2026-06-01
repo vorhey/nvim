4908104e193a214a1b09def22c623529065288bd
vorhey <jorgeherreraulloa@gmail.com>
2026-06-01 18:14:25 -0400

    updated neovide animation speed

--- lua/settings.lua
+++ lua/settings.lua
@@ -71,9 +71,6 @@ if not vim.g.neovide then
 end
 
 if vim.g.neovide then
-  vim.g.neovide_position_animation_length = 0.15
-  vim.g.neovide_scroll_animation_length = 0.5
-  vim.g.neovide_cursor_animation_length = 0.5
   vim.g.neovide_cursor_trail_size = 0.5
   vim.g.neovide_cursor_animate_in_insert_mode = true
   vim.g.neovide_cursor_animate_command_line = true
