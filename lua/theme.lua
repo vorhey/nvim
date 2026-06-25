a491ec4e18324d969801c89df929f75f7229e6bc
vorhey <jorgeherreraulloa@gmail.com>
2026-06-25 17:38:26 -0400

    remove matugen

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,36 +1,5 @@
 return {
   {
-    cond = function()
-      return vim.g.neovide
-    end,
-    'daedlock/matugen.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('matugen').setup {
-        colors_path = '~/.config/matugen/colors-empty.json',
-        contrast = 0.08,
-        -- min_contrast = 0.1,
-        brightness = -0.02,
-        -- sidebar_brightness = -0.04,
-      }
-      vim.cmd.colorscheme 'matugen'
-
-      local function transparent_statusline()
-        vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'NONE' })
-        vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'NONE' })
-        vim.api.nvim_set_hl(0, 'MiniStatuslineFilename', { bg = 'NONE' })
-        vim.api.nvim_set_hl(0, 'MiniStatuslineDevinfo', { bg = 'NONE' })
-        vim.api.nvim_set_hl(0, 'MiniStatuslineInactive', { bg = 'NONE' })
-      end
-      transparent_statusline()
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'MatugenReloaded',
-        callback = transparent_statusline,
-      })
-    end,
-  },
-  {
     'vorhey/ember.nvim',
     lazy = false,
     cond = function()
