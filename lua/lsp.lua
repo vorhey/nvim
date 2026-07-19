6c26c025d656cec420e259d7c2d6875a83c66d82
vorhey <jorgeherreraulloa@gmail.com>
2026-07-18 21:23:34 -0400

    fix php root_dir

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -455,9 +455,8 @@ return {
       filetypes = { 'php' },
       root_dir = function(bufnr, on_dir)
         local fname = vim.api.nvim_buf_get_name(bufnr)
-        local cwd = assert(vim.uv.cwd())
         local root = vim.fs.root(fname, { 'composer.json', '.git' })
-        on_dir(root and vim.fs.relpath(cwd, root) and cwd)
+        on_dir(root or vim.uv.cwd())
       end,
     })
 
