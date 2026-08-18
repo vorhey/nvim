9343c221395b3a2a6ef8cec47c7e48c8ae67c204
vorhey <jorgeherreraulloa@gmail.com>
2026-08-18 09:46:51 -0400

    removed neovide specific plugins

--- lua/custom/plugins/scope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'tiagovla/scope.nvim',
-  enabled = vim.g.neovide == true,
-  event = 'VeryLazy',
-  config = function()
-    vim.opt.sessionoptions:append { 'buffers', 'tabpages', 'globals' }
-    require('scope').setup {}
-  end,
-}
